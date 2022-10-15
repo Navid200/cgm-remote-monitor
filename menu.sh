92f5cab7ad23b7c499ea8f899a15b452cfe60942
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-15 18:01:15 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
-echo 
+echo
 echo "Bringing up the menu"
 echo
 
-# We need dialog for this.  This will be removed after installation of dialog is added to the main installation script.
-sudo apt-get update
-sudo apt-get -y install dialog
+# We need dialog for this.  This will be removed after installation of dialog is added to the main installation
+script.
+#sudo apt-get update
+#sudo apt-get -y install dialog
 
 clear #  Clear the screen before placing the next dialog on.
-dialog --radiolist "Choose one of the following options."\n\n\
-12 45 25\
-"Backup Mongo database" 1 "on"\
-"Restore a Mongo backup" 2 "off"\
-"Update executables" 3 "off"\
-"Install" 4 "off"\
-"Update/Customize Nightscout" 5 "off"
+
+Choice=$(dialog --menu "Choose one of the following options.\n\n" 15 50 7\
+ "1" "Backup Mongo database"\
+ "2" "Restore a Mongo backup"\
+ "3" "Transfer database from another server"\
+ "4" "Update executables"\
+ "5" "Reboot server"\
+ "6" "Install Nightscout"\
+ "7" "Update/Customize Nightscout" 3>&1 1>&2 2>&3)
+
+response=$?
+if [ $response = 255 ] #  Escape was chosen
+then
+clear # Clear the screen before exiting.
+echo "Escape"
+echo "Cannot continue."
+exit 5
+fi
+if [ $response = 1 ] #  Cancelled
+then
+clear # Clear the screen before exiting.
+echo "Cancel"
+echo "Cannot continue."
+exit 5
+fi
+
+echo "$Choice"
+
