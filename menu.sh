db5b35ea9aa31e531a390bafdb2717063be85c51
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-14 21:11:20 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -9,10 +9,10 @@ sudo apt-get update
 sudo apt-get -y install dialog
 
 clear #  Clear the screen before placing the next dialog on.
-dialog --radiolist "Choose one of the following options.\n\n
+dialog --radiolist "Choose one of the following options."\n\n\
 12 45 25\
-"Backup Mongo database" 1 on\
-"Restore a Mongo backup" 2 off\
-"Update executables" 3 off\
-"Install" 4 off\
-"Update/Customize Nightscout" 5 off
+"Backup Mongo database" 1 "on"\
+"Restore a Mongo backup" 2 "off"\
+"Update executables" 3 "off"\
+"Install" 4 "off"\
+"Update/Customize Nightscout" 5 "off"
