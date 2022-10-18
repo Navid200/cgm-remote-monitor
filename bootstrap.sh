b103f0d99eee7336b3ebdf26cff86199b3027593
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-18 15:07:30 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,32 +2,37 @@
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
 
 echo 
-echo "Bootstrapping Nightscout installer - JamOrHam"
+echo "Bootstrapping the menu - JamOrHam"
 echo
-cd /tmp
-if [ ! -s installation.sh ]
-then
+
 sudo apt-get update
 sudo apt-get -y install wget bash
-wget https://raw.githubusercontent.com/jamorham/nightscout-vps/vps-1/installation.sh
-if [ ! -s installation.sh ]
+sudo apt-get -y install dialog
+
+cd /tmp
+if [ ! -s menu.sh ]
 then
-echo "UNABLE TO DOWNLOAD INSTALLATION SCRIPT! - cannot continue - please try again!"
+
+wget https://raw.githubusercontent.com/Navid200/cgm-remote-monitor/Navid_2022_10_14c/menu.sh
+# wget https://raw.githubusercontent.com/jamorham/nightscout-vps/vps-1/menu.sh
+if [ ! -s menu.sh ]
+then
+echo "UNABLE TO DOWNLOAD MENU SCRIPT! - cannot continue - please try again!"
 exit 5
 fi
 fi
 
+sudo chmod 755 menu.sh
+/tmp/menu.sh
 
-if [ "$SSH_TTY" = "" ]
-then
-echo "Must be run from ssh session"
-exit 5
-fi
+#if [ "$SSH_TTY" = "" ]
+#then
+#echo "Must be run from ssh session"
+#exit 5
+#fi
 
-echo
-echo "Running installer"
-echo
-sudo < $SSH_TTY bash installation.sh
-echo
-echo "Finished running installer"
-echo
+#echo
+#echo "Running installer"
+#echo
+#sudo < $SSH_TTY bash installation.sh
+#echo
