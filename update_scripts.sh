c79fb05da8a03dfd998bc221c9670793877dd0d5
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-18 21:02:59 -0400

    Update update_scripts.sh

--- update_scripts.sh
+++ update_scripts.sh
@@ -19,24 +19,24 @@ if [ -s ./nightscout-vps ]
 
 then
 
-sudo rm -r nightscout-vps # If the directory already exists in the tmp directory, delete it.
-#sudo rm -r cgm-remote-monitor # If the directory already exists in the tmp directory, delete it.
+#sudo rm -r nightscout-vps # If the directory already exists in the tmp directory, delete it.  Main
+sudo rm -r cgm-remote-monitor # If the directory already exists in the tmp directory, delete it.  Navid's
 
 fi
 
-sudo git clone https://github.com/jamorham/nightscout-vps.git # Clone the install repository.
-#sudo git clone https://github.com/Navid200/cgm-remote-monitor.git # Clone the install repository.
+#sudo git clone https://github.com/jamorham/nightscout-vps.git # Clone the install repository.  Main
+sudo git clone https://github.com/Navid200/cgm-remote-monitor.git # Clone the install repository.  Navid's
 
-cd nightscout-vps
-#cd cgm-remote-monitor
+#cd nightscout-vps # Main
+cd cgm-remote-monitor # Navid's
 
-sudo git checkout vps-1
-#sudo git checkout Navid_2022_10_14c
+#sudo git checkout vps-1 # Main
+sudo git checkout Navid_2022_10_14c # Navid's
 
 sudo git pull
 sudo chmod 755 *.sh # Change premissions to allow execution by all.
 sudo mv -f *.sh /xDrip/scripts # Overwrite the scripts in the scripts directory with the new ones.
 cd ..
 
-sudo rm -r nightscout-vps # Delete the temporary pull directory.
-#sudo rm -r cgm-remote-monitor # Delete the temporary pull directory.
+#sudo rm -r nightscout-vps # Delete the temporary pull directory. # Main
+sudo rm -r cgm-remote-monitor # Delete the temporary pull directory. # Navid's
