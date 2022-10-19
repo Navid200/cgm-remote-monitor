0679ace9069cb8f6f6b001a8db9ea205c26fd76c
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-18 22:08:03 -0400

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -9,30 +9,36 @@ do
 clear #  Clear the screen before placing the next dialog on.
 
 Choice=$(dialog --nocancel --menu "Use the arrow keys to move the cursor.\n\
-Press Enter to execute the highlighted option.\n\n" 13 50 9\
+Press Enter to execute the highlighted option.\n\n" 15 50 9\
  "1" "Initial Nightscout install"\
- "2" "Transfer database from another server"\
- "3" "Update/Customize Nightscout"\
- "4" "Reboot server"\
- "5" "Terminal" 3>&1 1>&2 2>&3)
+ "2"
+ "3" "Transfer database from another server"\
+ "4" "Update/Customize Nightscout"\
+ "5" "Reboot server"\
+ "6" "Terminal" 3>&1 1>&2 2>&3)
 
 case $Choice in
 1)
 clear
-sudo /xDrip/scripts/installation.sh
+sudo /xDrip/scripts/NS_Install.sh
 ;;
 
 2)
 clear
-sudo /xDrip/scripts/clone_nightscout.sh
+sudo /xDrip/scriptes/NS_Install2.sh
 ;;
 
 3)
 clear
-sudo /xDrip/scripts/update_nightscout.sh
+sudo /xDrip/scripts/clone_nightscout.sh
 ;;
 
 4)
+clear
+sudo /xDrip/scripts/update_nightscout.sh
+;;
+
+5)
 dialog --yesno "Are you sure you want to reboot the server?\n
 If you do, all unsaved open files will close without saving.\n"  8 50
 response=$?
@@ -44,7 +50,7 @@ sudo reboot
 fi
 ;;
 
-5)
+6)
 # Clear before exiting. 
 clear
 exit
