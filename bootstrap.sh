782bca365fc47f82de56873da802bf704e36caf5
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-20 12:19:46 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,49 +29,49 @@ clear
 dialog --textbox bootstrap_note 13 53
 clear
 
-##cd /
-##if [ ! -s xDrip ]
-##then
-##sudo mkdir xDrip
-##fi
-##cd xDrip
-##if [ ! -s scripts ]
-##then
-##sudo mkdir scripts
-##fi
-##cd /tmp
-##if [ ! -s update_scripts.sh ]
-##then
-##wget https://raw.githubusercontent.com/Navid200/cgm-remote-monitor/Navid_2022_10_14c_Test/update_scripts.sh # Navid's
+cd /
+if [ ! -s xDrip ]
+then
+sudo mkdir xDrip
+fi
+cd xDrip
+if [ ! -s scripts ]
+then
+sudo mkdir scripts
+fi
+cd /tmp
+if [ ! -s update_scripts.sh ]
+then
+wget https://raw.githubusercontent.com/Navid200/cgm-remote-monitor/Navid_2022_10_14c_Test/update_scripts.sh # Navid's
 #wget https://raw.githubusercontent.com/jamorham/nightscout-vps/vps-1/update_scripts.sh # Main
-##if [ ! -s update_scripts.sh ]
-##then
-##echo "UNABLE TO DOWNLOAD update_scripts SCRIPT! - cannot continue - please try again!"
-##exit 5
-##fi
-##fi
+if [ ! -s update_scripts.sh ]
+then
+echo "UNABLE TO DOWNLOAD update_scripts SCRIPT! - cannot continue - please try again!"
+exit 5
+fi
+fi
 
-##sudo chmod 755 update_scripts.sh
-##sudo mv -f update_scripts.sh /xDrip/scripts
+sudo chmod 755 update_scripts.sh
+sudo mv -f update_scripts.sh /xDrip/scripts
 
 # Updating the scripts
-##sudo /xDrip/scripts/update_scripts.sh
+sudo /xDrip/scripts/update_scripts.sh
 
 # So that the menu comes up as soon as the user logs in (opens a terminal)
-##cd /tmp
-##cat > /tmp/start_menu.sh << EOF
-###!/bin/sh
-##/xDrip/scripts/menu.sh
+cd /tmp
+cat > /tmp/start_menu.sh << EOF
+#!/bin/sh
+/xDrip/scripts/menu.sh
 
-##EOF
-##sudo chown root:root start_menu.sh
-##sudo chmod 755 start_menu.sh
-##sudo mv -f start_menu.sh /etc/profile.d
+EOF
+sudo chown root:root start_menu.sh
+sudo chmod 755 start_menu.sh
+sudo mv -f start_menu.sh /etc/profile.d
 
-##cat > ~/.bash_aliases << EOF
-##alias menu="/xDrip/scripts/menu.sh"
+cat > ~/.bash_aliases << EOF
+alias menu="/xDrip/scripts/menu.sh"
 
-##EOF
+EOF
 
 # Reboot
 #sudo reboot
