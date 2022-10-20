96bc3b3b688e29d66fcc0dacf23cd9c143e89836
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-20 12:56:45 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,14 +13,14 @@ sudo apt-get -y install dialog
 sudo apt-get install -y  git python gcc g++ make
 
 cat > /tmp/bootstrap_note << EOF
-The next time you open a terminal, a menu will 
-offer you all the available options.
-
 In a few moments after you proceed, the server 
 will automatically reboot and an expected error
 message will appear.
 Please wait 20 seconds and then, click on "Retry"
 to reconnect.
+
+After this, every time you open a terminal, a
+menu will offer all the available options.
 
 To proceed, press Enter.
 
