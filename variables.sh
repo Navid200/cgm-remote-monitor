d68c685ea384aa1ef0a8e52406936da04f54e6af
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-21 19:57:57 -0400

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -4,8 +4,8 @@ echo
 echo "Nightscout variables"
 echo
 
-dialog --msgbox "You will be editing the textfile containing the variables.\n\
-The key combinations for each text editor function will be shown\n\
+dialog --msgbox "You will be editing the file containing the variables.\n\
+The key combinations for text editor functions will be shown\n\
 at the bottom of the screen.\n\
 ^ represents the control key.  Therefore, ^X means pressing\n\
 the control and x keys simultaneously.\n\n\
@@ -17,3 +17,4 @@ Press Enter now to proceed to the text editor." 18 67
 clear
 
 sudo nano /etc/nsconfig
+ 
