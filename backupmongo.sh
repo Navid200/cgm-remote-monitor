0aff286249f76e7eac40ec1d328f4c8d32a730ed
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-21 13:16:35 -0400

    Update backupmongo.sh

--- backupmongo.sh
+++ backupmongo.sh
@@ -9,8 +9,7 @@ EOF
 while :
 do
 exec 3>&1
-Filename=$(dialog --ok-label "Submit" --form "Enter a name for the backup file" 8 50 0 "file name" 1 1 "$filenam
-e" 1 14 25 0 2>&1 1>&3)
+Filename=$(dialog --ok-label "Submit" --form "Enter a name for the backup file" 8 50 0 "file name" 1 1 "$filename" 1 14 25 0 2>&1 1>&3)
  response=$?
 if [ $response = 255 ] || [ $response = 1 ]
 then
