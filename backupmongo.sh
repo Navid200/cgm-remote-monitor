8c7deb0d7a44a4ccbecc05caa814532623bff42c
Navid <51497406+Navid200@users.noreply.github.com>
2022-10-21 13:05:46 -0400

    Update backupmongo.sh

--- backupmongo.sh
+++ backupmongo.sh
@@ -2,23 +2,30 @@
 
 cat > /tmp/samefilename << EOF
 A file with the same name exists.
-Use a different filename.
+Choose a different filename.
 
 EOF
 
 while :
 do
 exec 3>&1
-Filename=$(dialog --ok-label "Submit" --form "Enter a name for the backup file" 12 50 0 "file name" 1 1 "$filename" 1 14 25 0 2>&1 1>&3) 
+Filename=$(dialog --ok-label "Submit" --form "Enter a name for the backup file" 8 50 0 "file name" 1 1 "$filenam
+e" 1 14 25 0 2>&1 1>&3)
  response=$?
-if [ ! $response = 255 ] & [ ! $response = 1 ]
+if [ $response = 255 ] || [ $response = 1 ]
 then
+clear
+exit
+fi
+
 if [ -s $Filename ]
 then
-dialog --textbox /tmp/samefilename 10 50
+dialog --exit-label "Try again" --textbox /tmp/samefilename 7 50
 else
-mongodump --gzip --archive=$Filename 
-exec 3>&- 
-fi
+mongodump --gzip --archive=$Filename
+exec 3>&-
+clear
+exit
 fi
 done
+ 
