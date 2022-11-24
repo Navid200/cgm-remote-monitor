6cff4c130349f64a89c17fc550a64d11c1c0a854
Navid <51497406+Navid200@users.noreply.github.com>
2022-11-24 00:03:04 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,9 +10,10 @@ if [ ! -z "$(ls /srv)" ]
 then
 clear
 dialog --colors --msgbox "     \Zr Developed by the xDrip team \Zn\n\n\
-The script you are running \"bootstrap\", is meant to initiate an installtion.  \
-However, the file system does not seem to be empty.  If you already have an installtion on this machine and \
-proceed by pressing enter, you will be modifying it.  If that's not your intention, please press escape to abort." 13 50
+The script you are running, \"bootstrap\", is meant to initiate an installtion.  \
+However, the file system does not seem to be empty.\n\
+If you already have an installtion on this machine and \
+proceed by pressing enter, you will be modifying it.  If that's not your intention, please press escape to abort." 14 50
 if [ $? -eq 255 ]
 then
 exit
