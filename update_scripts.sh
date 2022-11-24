129f6d85dd5aec7070d432d49d286b9cdcaf8555
Navid <51497406+Navid200@users.noreply.github.com>
2022-11-23 23:24:43 -0500

    Update update_scripts.sh

--- update_scripts.sh
+++ update_scripts.sh
@@ -4,43 +4,16 @@ echo
 echo "Fetch the latest scripts from GitHub - Navid200"
 echo
 
-Test=0
-# Comment out the next line before submission.
-#Test=1 ########################### This line must be commented out before submitting a PR #########
-
-cd /tmp
-
-if [ $Test -gt 0 ] # Are we testing?  
-then
-if [ -s ./cgm-remote-monitor ]
-then
-sudo rm -r cgm-remote-monitor
-fi
-sudo git clone https://github.com/Navid200/cgm-remote-monitor.git # Test
-cd cgm-remote-monitor
-sudo git checkout Navid_2022_11_16_Test
-else # If we are not testing
-if [ -s ./nightscout-vps ]
-then
-sudo rm -r nightscout-vps
-fi
-sudo git clone https://github.com/jamorham/nightscout-vps.git #  Main
-cd nightscout-vps
-sudo git checkout vps-1
-fi
-
+cd /xDrip/clone
+cd "$(< repo)"  # Go to the 
 sudo git pull
+
 sudo chmod 755 *.sh # Change premissions to allow execution by all.
 rm -f /xDrip/scripts/*.sh # Remove the existing sh files
-sudo mv -f *.sh /xDrip/scripts # Overwrite the scripts in the scripts directory with the new ones.
+sudo cp *.sh /xDrip/scripts # Overwrite the scripts in the scripts directory with the new ones.
 rm -rf /xDrip/ConfigServer # Remove the existing ConfigServer directory
-sudo mv ConfigServer /xDrip/.
+sudo cp -r ConfigServer /xDrip/.
 cd ..
-sudo rm -rf nightscout-vps 
-sudo rm -rf cgm-remote-monitor
-
-# Update Configserver
-
 
 if [ ! -s /tmp/nodialog_update_scripts ]
 then
