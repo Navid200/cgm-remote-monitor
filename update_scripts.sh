8f2e537a1c4134099adc62f250df1e862fb5f674
Navid <51497406+Navid200@users.noreply.github.com>
2022-11-24 19:43:38 -0500

    Update update_scripts.sh

--- update_scripts.sh
+++ update_scripts.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# This should never be called before first being updated to the latest remote release.
+# Currently, only bootstrap and menu call this.  Both update this file before calling it.
+# If you decide to call this anywhere else, make sure to update the local copy before calling it.
+
 echo
 echo "Fetch the latest scripts from GitHub - Navid200"
 echo
 
 cd /srv
 cd "$(< repo)" 
-sudo git reset --hard
-sudo git pull
+sudo git reset --hard  # delete any local edits.
+sudo git pull  # Update database from remote.
 
 sudo chmod 755 *.sh # Change premissions to allow execution by all.
 rm -f /xDrip/scripts/*.sh # Remove the existing sh files
