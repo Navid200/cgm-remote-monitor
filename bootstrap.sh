8f7ce02b80a865d7ff657f3598663db60a0425e8
Navid <51497406+Navid200@users.noreply.github.com>
2022-11-24 22:25:31 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
-# curl https://raw.githubusercontent.com/Navid200/cgm-remote-monitor/TestMethod/bootstrap.sh | bash
+# curl https://raw.githubusercontent.com/Navid200/cgm-remote-monitor/vps-1/bootstrap.sh | bash
 
 echo 
 echo "Bootstrapping the installation files - Navid200"
