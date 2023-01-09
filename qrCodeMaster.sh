3fe00e30444d2b094b857f5a4592a7d777317598
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-09 13:08:19 -0500

    Create qrCodeMaster.sh

---
+++ qrCodeMaster.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo
+echo "Show the base URL required to become master" - Navid200
+echo
+
+HOSTNAME=""
+. /etc/free-dns.sh
+
+. /etc/nsconfig
+apisec=$API_SECRET
+
+if [ "$(node -v)" = "" ] || [ "$HOSTNAME" = "" ] || [ "$apisec" = "" ] # If Node.js is not installed or there is no hostname or password
+then
+clear
+dialog --colors --msgbox "     \Zr Developed by the xDrip team \Zn\n\n\
+You need to complete Nightscout installation and have a hostname and API_SECRET." 9 50
+exit
+fi
+
+baseurl="$apisec @ $HOSTNAME /api/v1/"
+clear
+echo "$baseurl"
