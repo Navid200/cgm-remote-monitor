f1ec5d8c37e1de813cf1b5fc85107a7afcaf5d37
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-09 10:10:09 -0500

    Update Utilities.sh

--- Utilities.sh
+++ Utilities.sh
@@ -24,7 +24,7 @@ case $Choice in
 
 1)
 clear
-dialog --colors --textbox /xDrip/Logs 26 80
+dialog --colors --msgbox "     \Zr Developed by the xDrip team \Zn\n\n"  --textbox /xDrip/Logs 26 80 
 
 # "     \Zr Developed by the xDrip team \Zn\n\n\
 
