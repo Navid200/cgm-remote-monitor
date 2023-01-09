4eb38bd522cb182703962da546159751a1896ae1
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-08 23:32:35 -0500

    Update Utilities.sh

--- Utilities.sh
+++ Utilities.sh
@@ -13,40 +13,57 @@ Choice=$(dialog --colors --nocancel --nook --menu "\
   \n\n
 Use the arrow keys to move the cursor.\n\
 Press Enter to execute the highlighted option.\n\n" 20 50 6\
- "1" "Status"\
- "2" "Edit variables using a test editor"\
- "3" "Copy data from another Nightscout"\
- "4" "FreeDNS Setup"\
- "5" "Customize Nightscout"\
- "6" "Return"\
+ "1" "Logs"\
+ "2" "Installation phase 1 - 15 minutes"\
+ "3" "Installation phase 2 - 5 minutes"\
+ "4" "Edit variables using a text editor"\
+ "5" "Copy data from another Nightscout"\
+ "6" "FreeDNS Setup"\
+ "7" "Customize Nightscout"\
+ "8" "Bootstrap"\
  3>&1 1>&2 2>&3)
+ response=$?
+ if [ $response = 255 ] # escaped
+ then
+   clear
+   exit 5
+ fi
 
 case $Choice in
 
 1)
-/xDrip/scripts/Status.sh
+clear
+dialog 
 ;;
 
 2)
-/xDrip/scripts/variables.sh
+sudo /xDrip/scripts/NS_Install.sh
 ;;
 
 3)
+sudo /xDrip/scripts/NS_Install2.sh
+;;
+
+4)
+/xDrip/scripts/variables.sh
+;;
+
+5)
 clear
 sudo /xDrip/scripts/clone_nightscout.sh
 ;;
 
-4)
+6)
 clear
 sudo /xDrip/scripts/ConfigureFreedns.sh
 ;;
 
-5)
+7)
 sudo /xDrip/scripts/update_nightscout.sh
 ;;
 
-6)
-/xDrip/scripts/menu.sh
+8)
+curl https://raw.githubusercontent.com/jamorham/nightscout-vps/vps-1/bootstrap.sh | bash
 ;;
 
 esac
