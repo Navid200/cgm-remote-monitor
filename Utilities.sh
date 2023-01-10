70fc356b94dd9c64deddebb330908c0b1630fb4a
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-09 22:09:26 -0500

    Update Utilities.sh

--- Utilities.sh
+++ Utilities.sh
@@ -9,59 +9,51 @@ Choice=$(dialog --colors --nocancel --nook --menu "\
       \Zr Developed by the xDrip team \Zn\
   \n\n
 Use the arrow keys to move the cursor.\n\
-Press Enter to execute the highlighted option.\n\n" 18 50 8\
- "1" "Logs"\
- "2" "Installation phase 1 - 15 minutes"\
- "3" "Installation phase 2 - 5 minutes"\
- "4" "QR code to make xDrip master"\
- "5" "Edit variables using a text editor"\
- "6" "Copy data from another Nightscout"\
- "7" "FreeDNS Setup"\
- "8" "Customize Nightscout"\
- "9" "Bootstrap"\
+Press Enter to execute the highlighted option.\n\
+Press escape to return to the main menu\n" 19 50 8\
+ "1" "Installation phase 1 - 15 minutes"\
+ "2" "Installation phase 2 - 5 minutes"\
+ "3" "QR code to make xDrip master"\
+ "4" "Edit variables using a text editor"\
+ "5" "Copy data from another Nightscout"\
+ "6" "FreeDNS Setup"\
+ "7" "Customize Nightscout"\
+ "8" "Bootstrap"\
  3>&1 1>&2 2>&3)
 
 case $Choice in
 
 1)
-clear
-dialog --colors --title "\Zr Developed by the xDrip team \Zn"   --textbox /xDrip/Logs 26 74 
-
-# "     \Zr Developed by the xDrip team \Zn\n\n\
-
-;;
-
-2)
 sudo /xDrip/scripts/NS_Install.sh
 ;;
 
-3)
+2)
 sudo /xDrip/scripts/NS_Install2.sh
 ;;
 
-4)
+3)
 /xDrip/scripts/qrCodeMaster.sh
 ;;
 
-5)
+4)
 /xDrip/scripts/variables.sh
 ;;
 
-6)
+5)
 clear
 sudo /xDrip/scripts/clone_nightscout.sh
 ;;
 
-7)
+6)
 clear
 sudo /xDrip/scripts/ConfigureFreedns.sh
 ;;
 
-8)
+7)
 sudo /xDrip/scripts/update_nightscout.sh
 ;;
 
-9)
+8)
 curl https://raw.githubusercontent.com/jamorham/nightscout-vps/vps-1/bootstrap.sh | bash
 ;;
 
