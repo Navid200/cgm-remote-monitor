11fbfd617442d3f866d79be9e9029cc0528b419f
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-11 22:01:19 -0500

    Create update_postNS_packages.sh

---
+++ update_postNS_packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo
+echo "Install packages that are installed after Nightscout only if they are not installed already. - Navid200"
+echo
+
+# Let's install the missing needed packages.
+
+# node
+whichpack=$(node -v)
+if [ ! "$whichpack" = "v16.*" ]
+then
+  curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
+  sudo apt-get install -y nodejs
+fi 
+
+# Add log
+rm -rf /tmp/Logs
+echo -e "The post-NS packages have been updated     $(date)\n" | cat - /xDrip/Logs > /tmp/Logs
+sudo /bin/cp -f /tmp/Logs /xDrip/Logs
+  
