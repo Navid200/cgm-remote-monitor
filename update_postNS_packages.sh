7cbae8ca80672fe57c50f0f856fe4db59512b1bb
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-11 20:54:14 -0500

    Update update_postNS_packages.sh

--- update_postNS_packages.sh
+++ update_postNS_packages.sh
@@ -13,4 +13,9 @@ then
   curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
   sudo apt-get install -y nodejs
 fi 
+
+# Add log
+rm -rf /tmp/Logs
+echo -e "The post-NS packages have been updated     $(date)\n" | cat - /xDrip/Logs > /tmp/Logs
+sudo /bin/cp -f /tmp/Logs /xDrip/Logs
   
