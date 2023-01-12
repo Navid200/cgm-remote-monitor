086fdd80109785f0dd131b229bea208e8692916a
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-11 22:00:32 -0500

    Create update_packages.sh

---
+++ update_packages.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+echo
+echo "Install packages only if they are not installed already. - Navid200"
+echo
+
+# Let's install the missing needed packages.
+
+sudo apt-get update
+
+# vis
+whichpack=$(which vis)
+if [ "$whichpack" = "" ]
+then
+  sudo apt-get -y install vis
+fi
+
+# nano
+whichpack=$(which nano)
+if [ "$whichpack" = "" ]
+then
+  sudo apt-get -y install nano
+fi
+
+# screen
+whichpack=$(which screen)
+if [ "$whichpack" = "" ]
+then
+  sudo apt-get -y install screen
+fi
+
+# jq
+whichpack=$(which jq)
+if [ "$whichpack" = "" ]
+then
+  sudo apt-get -y install jq
+fi
+
+# qrencode
+whichpack=$(which qrencode)
+if [ "$whichpack" = "" ]
+then
+  sudo apt-get -y install qrencode
+fi
+
+# mongo
+whichpack="$(mongod --version | sed -n 1p)"
+if [ ! "$whichpack" = "db version v3.*" ]
+then
+  sudo apt-get -y install mongodb-server
+fi
+
+# git
+whichpack="$(git version)"
+if [ ! "$whichpack" = "git version 2.*" ]
+then
+  sudo apt-get install -y  git python gcc g++ make
+fi
+
+sudo apt-get install -y nodejs npm
+sudo apt -y autoremove
+
+# Add log
+rm -rf /tmp/Logs
+echo -e "The packages have been updated     $(date)\n" | cat - /xDrip/Logs > /tmp/Logs
+sudo /bin/cp -f /tmp/Logs /xDrip/Logs
+ 
