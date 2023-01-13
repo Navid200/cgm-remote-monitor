42acabafd5891d1914eedeff3e4c93039eef02c9
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-12 22:29:09 -0500

    Update Utilities.sh

--- Utilities.sh
+++ Utilities.sh
@@ -13,12 +13,10 @@ Press Enter to execute the highlighted option.\n\
 Press escape to return to the main menu\n" 19 50 8\
  "1" "Installation phase 1 - 15 minutes"\
  "2" "Installation phase 2 - 5 minutes"\
- "3" "QR code to make xDrip master"\
  "4" "Edit variables using a text editor"\
  "5" "Copy data from another Nightscout"\
  "6" "FreeDNS Setup"\
  "7" "Customize Nightscout"\
- "8" "Bootstrap"\
  3>&1 1>&2 2>&3)
 
 case $Choice in
@@ -29,10 +27,6 @@ sudo /xDrip/scripts/NS_Install.sh
 
 2)
 sudo /xDrip/scripts/NS_Install2.sh
-;;
-
-3)
-/xDrip/scripts/qrCodeMaster.sh
 ;;
 
 4)
@@ -51,10 +45,6 @@ sudo /xDrip/scripts/ConfigureFreedns.sh
 
 7)
 sudo /xDrip/scripts/update_nightscout.sh
-;;
-
-8)
-curl https://raw.githubusercontent.com/jamorham/nightscout-vps/vps-1/bootstrap.sh | bash
 ;;
 
 esac
