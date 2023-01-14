a310f197489ce2bee48c0a630b98567996a0cf9e
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-13 21:55:14 -0500

    Update menu_Data.sh

--- menu_Data.sh
+++ menu_Data.sh
@@ -6,11 +6,10 @@ echo
 
 clear
 Choice=$(dialog --colors --nocancel --nook --menu "\
-      \Zr Developed by the xDrip team \Zn\
-  \n\n
+       \Zr Developed by the xDrip team \Zn\n\n\
 Use the arrow keys to move the cursor.\n\
 Press Enter to execute the highlighted option.\n\
-Press escape to return to the main menu\n" 16 50 4\
+Press escape to return to the main menu\n" 15 50 3\
  "1" "Copy data from another Nightscout"\
  "2" "Backup MongoDB"\
  "3" "Restore MongoDB"\
