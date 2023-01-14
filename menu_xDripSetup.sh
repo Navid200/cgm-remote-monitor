bc62dc07626f05c7b13c7efa438632817d59c1e7
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-13 21:33:54 -0500

    Update menu_xDripSetup.sh

--- menu_xDripSetup.sh
+++ menu_xDripSetup.sh
@@ -9,7 +9,7 @@ Choice=$(dialog --colors --nocancel --nook --menu "\
       \Zr Developed by the xDrip team \Zn\n\n\
 Use the arrow keys to move the cursor.\n\
 Press Enter to execute the highlighted option.\n\
-Press escape to return to the main menu\n" 19 50 8\
+Press escape to return to the main menu\n" 14 50 3\
  "1" "QR code to make xDrip master"\
  3>&1 1>&2 2>&3)
 
