3a75c991444bb1ae2bd7997f32846dc2d7781f1a
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-13 22:17:47 -0500

    Update menu_NS_setup.sh

--- menu_NS_setup.sh
+++ menu_NS_setup.sh
@@ -9,8 +9,7 @@ Choice=$(dialog --colors --nocancel --nook --menu "\
         \Zr Developed by the xDrip team \Zn\
   \n\n
 Use the arrow keys to move the cursor.\n\
-Press Enter to execute the highlighted option.\n\
-Press escape to return to the main menu\n" 15 50 4\
+Press Enter to execute the highlighted option.\n" 14 50 4\
  "1" "Edit variables using a text editor"\
  "2" "Edit variables using a web browser"\
  "3" "Customize Nightscout"\
