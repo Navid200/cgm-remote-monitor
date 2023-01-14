79abc38ca1de29c1e125a0d40555c1017fe94b15
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-13 21:24:50 -0500

    Update menu_NS_setup.sh

--- menu_NS_setup.sh
+++ menu_NS_setup.sh
@@ -11,7 +11,6 @@ Choice=$(dialog --colors --nocancel --nook --menu "\
 Use the arrow keys to move the cursor.\n\
 Press Enter to execute the highlighted option.\n\
 Press escape to return to the main menu\n" 19 50 8\
-
  "1" "Edit variables using a text editor"\
  "2" "Edit variables using a web browser"\
  "3" "Customize Nightscout"\
