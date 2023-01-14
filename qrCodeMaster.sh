7694f0bbc0ee851b563bb913c373f33260541162
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-13 22:31:52 -0500

    Update qrCodeMaster.sh

--- qrCodeMaster.sh
+++ qrCodeMaster.sh
@@ -13,8 +13,8 @@ apisec=$API_SECRET
 if [ "$(node -v)" = "" ] || [ "$HOSTNAME" = "" ] || [ "$apisec" = "" ] # If Node.js is not installed or there is no hostname or password
 then
 clear
-dialog --colors --msgbox "     \Zr Developed by the xDrip team \Zn\n\n\
-You need to complete Nightscout installation and have a hostname and API_SECRET to show a QR code for setting up xDrip as an uploader." 9 50
+dialog --colors --msgbox "       \Zr Developed by the xDrip team \Zn\n\n\
+You need to complete Nightscout installation and have a hostname and API_SECRET to be able to show a QR code for setting up xDrip as an uploader." 10 50
 exit
 fi
 
