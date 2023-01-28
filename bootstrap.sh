4ec65096b222d38c5f4307d8890397a8f66d9b20
Navid <51497406+Navid200@users.noreply.github.com>
2023-01-27 20:34:03 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -109,11 +109,11 @@ EOF
 fi
 
 if [ "$ExistingSystem" = "0" ]  # If this is not an existing installation
+then
   clear
   dialog --colors --msgbox "     \Zr Developed by the xDrip team \Zn\n\n\
 If any item above the line on the status page (shown next) is red, it represents an incorrect parameter that could result in malfunction or cost.  \
 Please take a note, delete the virtual machine, and create a new one.   For more detail, please refer to the guide." 13 50
-
 else
   dialog --colors --msgbox "     \Zr Developed by the xDrip team \Zn\n\n\Press enter to return to the status page." 12 50
 fi
