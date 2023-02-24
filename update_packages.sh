ec7569ebaabf97e296eef236e28917a47d95b4ba
Navid <51497406+Navid200@users.noreply.github.com>
2023-02-23 22:57:32 -0500

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -65,6 +65,8 @@ then
   sudo apt-get -y install file
 fi  
 
+# The last item on the above list of packages must be verified in Status.sh to have been installed.  
+
 # Add log
 rm -rf /tmp/Logs
 echo -e "The packages have been installed     $(date)\n" | cat - /xDrip/Logs > /tmp/Logs
