2827cbe11406c1a62fecf8604d948de7447660fe
Navid <51497406+Navid200@users.noreply.github.com>
2023-04-22 20:03:36 -0400

    Update Status.sh

--- Status.sh
+++ Status.sh
@@ -130,10 +130,10 @@ then
 fi  
 
 # Verify that Nightscout will start after a reboot even if FreeDNS is down.
-rclocal1=""
-if grep -q "DIRECTURL" /etc/rc.local
+rclocal1="\Zb\Z1Startup dependence on FreeDNS\Zn"
+if grep -q "freedns -eq 200" /etc/rc.local
 then
-  rclocal1="\Zb\Z1Startup dependence on FreeDNS\Zn"
+  rclocal1=""
 fi
 
 clear
