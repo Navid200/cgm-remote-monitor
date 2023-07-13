e9cb692799816472c2d0e1f07f9d18b0548a5edd
Navid <51497406+Navid200@users.noreply.github.com>
2023-07-13 08:40:19 -0400

    Update FreednsLogin.sh

--- FreednsLogin.sh
+++ FreednsLogin.sh
@@ -4,10 +4,11 @@ echo
 echo "Log into the FreeDNS site. - Navid200"
 echo
 
+# This could also run in the background.  So, it should contain no dialog.
+
 freedns=$(wget --spider -S "https://freedns.afraid.org/" 2>&1 | awk '/HTTP\// {print $2}') # This will be 200 if FreeDNS is up.
 if [ $freedns -eq 200 ]  # Run the following only if FreeDNS is up.
-then
-  # This could also run in the background.  So, it should contain no dialog.  
+then 
   if [ -s /xDrip/FreeDNS_ID_Pass ] # If the FreeDNS_ID_Pass file exists
   then
     . /xDrip/FreeDNS_ID_Pass
@@ -25,12 +26,15 @@ then
     else
       echo -e "Logged into FreeDNS.      $(date)\n" | cat - /xDrip/FreeDNS_AutoLogin_Logs > /tmp/Logs
     fi
-    sudo /bin/cp -f /tmp/Logs /xDrip/FreeDNS_AutoLogin_Logs
   else
     # Add log
     rm -rf /tmp/Logs
     echo -e "The /xDrip/FreeDNS_ID_Pass file does not exist.      $(date)\n" | cat - /xDrip/FreeDNS_AutoLogin_Logs > /tmp/Logs
-    sudo /bin/cp -f /tmp/Logs /xDrip/FreeDNS_AutoLogin_Logs
   fi
+else
+  # Add log.
+  rm -rf /tmp/Logs
+  echo -e "The FreeDNS site seems to be down.      $(date)\n" | cat - /xDrip/FreeDNS_AutoLogin_Logs > /tmp/Logs
 fi
+sudo /bin/cp -f /tmp/Logs /xDrip/FreeDNS_AutoLogin_Logs
  
