492001fa2425c1360124c02d1105a3f7b63a79fe
Navid <51497406+Navid200@users.noreply.github.com>
2023-07-13 00:01:26 -0400

    Update AutoLoginFreedns.sh

--- AutoLoginFreedns.sh
+++ AutoLoginFreedns.sh
@@ -4,17 +4,19 @@ echo
 echo "Log into the FreeDNS site in the background. - Navid200"
 echo
 
-# This could also run in the background.  So, it should contain no dialog.  
-
-
-echo "Installing Nightscout"
-cd "$(< repo)" 
-sudo git reset --hard  # delete any local edits.
-sudo git pull  # Update database from remote.
-
-
-# Add log
-rm -rf /tmp/Logs
-echo -e "Logged into FreeDNS.      $(date)\n" | cat - /xDrip/Logs > /tmp/Logs
-sudo /bin/cp -f /tmp/Logs /xDrip/Logs
+freedns=$(wget --spider -S "https://freedns.afraid.org/" 2>&1 | awk '/HTTP\// {print $2}') # This will be 200 if FreeDNS is up.
+if [ $freedns -eq 200 ]  # Run the following only if FreeDNS is up.
+then
+  # This could also run in the background.  So, it should contain no dialog.  
+  if [ -s /xDrip/FreeDNS_ID_Pass ] # If the FreeDNS_ID_Pass file exists
+  then
+    . /xDrip/FreeDNS_ID_Pass
+    user=$User_ID
+    pass=$Password
+    # Add log
+    rm -rf /tmp/Logs
+    echo -e "Logged into FreeDNS.      $(date)\n" | cat - /xDrip/FreeDNS_AutoLogin_Logs > /tmp/Logs
+    sudo /bin/cp -f /tmp/Logs /xDrip/FreeDNS_AutoLogin_Logs
+  fi
+fi
  
