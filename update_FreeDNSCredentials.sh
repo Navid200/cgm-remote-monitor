049e0f991fffed2507d6aa68f28125cc4a6cac54
Navid <51497406+Navid200@users.noreply.github.com>
2023-07-16 10:11:09 -0400

    Update update_FreeDNSCredentials.sh

--- update_FreeDNSCredentials.sh
+++ update_FreeDNSCredentials.sh
@@ -17,7 +17,7 @@ then
   go_back=0
   exec 3>&1
   Values=$(dialog --colors --ok-label "Submit" --form "       \Zr Developed by the xDrip team \Zn\n\n\n\
-This utility allows you to enter your FreeDNS user ID and password in Google Cloud Nightscout.\n\
+This utility lets you enter your FreeDNS user ID and password in Google Cloud Nightscout.\n\
 It does not change your FreeDNS user ID or password.  To do that, you will need to use a browser and log into FreeDNS. Then, use this utility to update Google Cloud Nightscout accordingly.\n\n\
 Enter your ID and password to proceed.  Or press escape to cancel." 20 50 0 "User ID:" 1 1 "$user" 1 14 25 0 "Password:" 2 1 "$pass" 2 14 25 0 2>&1 1>&3)
   response=$?
