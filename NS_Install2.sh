3eebca1a0de78e7ac49a144de40dcb789b2e852b
Navid <51497406+Navid200@users.noreply.github.com>
2023-08-31 13:53:02 -0400

    Update NS_Install2.sh

--- NS_Install2.sh
+++ NS_Install2.sh
@@ -100,8 +100,8 @@ clear
 exec 3>&1
 Value=$(dialog --colors --ok-label "Submit" --form "       \Zr Developed by the xDrip team \Zn\n\n\n\
 API_SECRET:   $cs\n\n\
-Press escape to keep it, or enter a new one with at least 12 characters excluding the following.\n\
-  $   \"   '   \\   SPACE   @   / " 15 50 0 "API_SECRET:" 1 1 "$secr" 1 14 25 0 2>&1 1>&3)
+Press escape to keep it, or enter a new API_SECRET with at least 12 characters excluding the following.\n\
+  $   \"   '   \\   SPACE   @   / " 16 50 0 "API_SECRET:" 1 1 "$secr" 1 14 25 0 2>&1 1>&3)
 response=$?
 if [ $response = 255 ] || [ $response = 1 ] # cancled or escaped
 then
