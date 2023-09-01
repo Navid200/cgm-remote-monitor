07ebb9d0d82dd8bdf64360f05e4a1e952de54a62
Navid <51497406+Navid200@users.noreply.github.com>
2023-08-31 22:04:26 -0400

    Update Status.sh

--- Status.sh
+++ Status.sh
@@ -155,10 +155,10 @@ fi
 # Mark problem characters in API_SECRET
 apisec_literal=$(grep 'API_SECRET=' /etc/nsconfig | sed 's/^.*=//')
 apisec_problem=""
-#if [[ "$apisec" == *@* ]] || [[ "$apisec" == *\ * ]] || [[ "$apisec" == */* ]] || [[ "$apisec" == *\\* ]]
-#then
-#  apisec_problem="\Zb\Z5*\Zn"
-#fi
+if [[ "$apisec" == *@* ]] || [[ "$apisec" == *\ * ]] || [[ "$apisec" == */* ]] || [[ "$apisec" == *\\* ]]
+then
+  apisec_problem="\Zb\Z5*\Zn"
+fi
 
 clear
 Choice=$(dialog --colors --nocancel --nook --menu "\
@@ -172,7 +172,7 @@ Ubuntu: $ubuntu \n\
 HTTP & HTTPS:  $http \n\
 ------------------------------------------ \n\
 Google Cloud Nightscout  2023.08.31\n\
-$Missing $Phase1 $rclocal_1 $freedns_id_pass $apisec_literal \n\n\
+$Missing $Phase1 $rclocal_1 $freedns_id_pass $apisec_literal $apisec_problem \n\n\
 /$uname/$repo/$branch\n\
 Swap: $swap \n\
 Mongo: $mongo \n\
