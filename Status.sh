f92eedad54c657fab46f9ce0873a5e87dafc1104
Navid <51497406+Navid200@users.noreply.github.com>
2023-08-31 22:13:41 -0400

    Update Status.sh

--- Status.sh
+++ Status.sh
@@ -153,7 +153,7 @@ then
 fi
 
 # Mark problem characters in API_SECRET
-apisec_literal="$(grep 'API_SECRET=' /etc/nsconfig | sed 's/^.*=//')"
+apisec_literal=$(grep 'API_SECRET=' /etc/nsconfig | sed 's/^.*=//')
 apisec_problem=""
 if [[ "$apisec" == *@* ]] || [[ "$apisec" == *\ * ]] || [[ "$apisec" == */* ]] || [[ "$apisec" == *\\* ]]
 then
