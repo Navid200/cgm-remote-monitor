6095cb9792d8fc538b1d36054e817e18ffffaf24
Navid <51497406+Navid200@users.noreply.github.com>
2023-09-01 07:52:39 -0400

    Update Status.sh

--- Status.sh
+++ Status.sh
@@ -158,10 +158,10 @@ apisec_literal=$(grep 'API_SECRET=' /etc/nsconfig | sed 's/^.*=//')
 apisec_literal="$apisec_literal"
 apisec_literal="${apisec_literal:1: -1}"
 apisec_problem=""
-#if [[ "$apisec" == *@* ]] || [[ "$apisec" == *\ * ]] || [[ "$apisec" == */* ]] || [[ "$apisec" == *\\* ]]
-#then
-#  apisec_problem="\Zb\Z5*\Zn"
-#fi
+if [[ "$apisec_literal" == *@* ]] || [[ "$apisec_literal" == *\ * ]] || [[ "$apisec_literal" == */* ]] || [[ "$apisec" == *\\* ]]
+then
+  apisec_problem="\Zb\Z5*\Zn"
+fi
 
 clear
 Choice=$(dialog --colors --nocancel --nook --menu "\
