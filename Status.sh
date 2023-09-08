31c349b362783b6a99d4814f4d2b2e4961f6b845
Navid <51497406+Navid200@users.noreply.github.com>
2023-09-08 00:04:51 -0400

    Update Status.sh

--- Status.sh
+++ Status.sh
@@ -166,7 +166,7 @@ first="${apisec_literal:0:1}"
 last="${apisec_literal: -1}"
 if [[ "$first" == "$last" ]] # Are the first and last characters identical?
 then
-  first="1"
+apisec_problem="*45"
 else
   apisec_problem="*"
 fi
