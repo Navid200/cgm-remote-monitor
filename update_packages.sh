07217aa08b8d23080b06e9e4c9a6672b34e8f6fb
Navid <51497406+Navid200@users.noreply.github.com>
2023-10-23 16:39:23 -0400

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -63,6 +63,7 @@ fi
 whichpack=$(node -v)
 if [ ! "${whichpack%%.*}" = "v16" ]
 then
+  sudo apt-get update
   sudo apt-get install -y ca-certificates curl gnupg
   sudo mkdir -p /etc/apt/keyrings
   curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
