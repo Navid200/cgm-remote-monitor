46bc413bd6f89e1382733402a54ec6c31b70738e
Navid <51497406+Navid200@users.noreply.github.com>
2023-10-23 16:20:09 -0400

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -63,8 +63,13 @@ fi
 whichpack=$(node -v)
 if [ ! "${whichpack%%.*}" = "v16" ]
 then
-  curl -s https://deb.nodesource.com/setup_16.x | sudo bash
-  sudo apt install nodejs -y
+  sudo apt-get install -y ca-certificates curl gnupg
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+  NODE_MAJOR=16
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+  sudo apt-get update
+  sudo apt-get install nodejs -y
 fi 
 
 # file
