c5be59719ff1f1acbdb82341e51b26ab59bb11f7
Navid <51497406+Navid200@users.noreply.github.com>
2023-10-28 18:11:14 -0400

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -61,11 +61,17 @@ fi
 
 # node
 whichpack=$(node -v)
-if [ ! "${whichpack%%.*}" = "v14" ]
+if [ ! "${whichpack%%.*}" = "v18" ]
 then
-  curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash - &&\
-  sudo apt-get install -y nodejs
-fi 
+  sudo apt-get update
+  sudo apt-get install -y ca-certificates curl gnupg
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+  NODE_MAJOR=18
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+  sudo apt-get update
+  sudo apt-get install nodejs -y
+fi
 
 # file
 whichpack=$(which file)
