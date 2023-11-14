9110c55f23b6ba475de613e6c5627c97725fcfd7
Navid <51497406+Navid200@users.noreply.github.com>
2023-11-13 22:03:13 -0500

    Update Status.sh

--- Status.sh
+++ Status.sh
@@ -202,7 +202,7 @@ $apisec_problem $Missing $Phase1 $rclocal_1 $freedns_id_pass \n\n\
 /$uname/$repo/$branch\n\
 Swap: $swap \n\
 Mongo: $mongo \n\
-Security update: $LastReboot \n\
+Ubuntu update: $LastReboot \n\
 NS proc: $ns \n\
 FreeDNS name and IP: $FD \n\
 Certificate: $cert \
