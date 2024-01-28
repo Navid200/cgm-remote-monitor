4ad9d30bd03ee6bbe2c3b8128f51414eeb3efe40
Navid <51497406+Navid200@users.noreply.github.com>
2024-01-28 12:35:22 -0500

    Create AddLog.sh

---
+++ AddLog.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo
+echo "Log function" - Navid200
+echo
+
+# Requires two parameters.
+# The first parameter is a line script that will be added to the log file.
+# The second parameter is the full path to the log file.
+
+if [ $3 != Null ]
+then
+echo "Error: More than 2 parameters"
+fi
