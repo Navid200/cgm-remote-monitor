e378c869c3f4446f73b8ffad3a19bae9088aa021
Navid <51497406+Navid200@users.noreply.github.com>
2024-01-28 15:44:31 -0500

    Update AddLog.sh

--- AddLog.sh
+++ AddLog.sh
@@ -8,7 +8,13 @@ echo
 # The first parameter is a line script that will be added to the log file.
 # The second parameter is the full path to the log file.
 
-if [ $3 != Null ]
+if [ $# != 2 ]
 then
-echo "Error: More than 2 parameters"
+  echo "Error: Incorrect number of parameters"
+  exit 5
 fi
+
+sudo rm -rf /tmp/Logs
+echo -e "$1     $(date)\n" | cat - /xDrip/Logs > /tmp/Logs
+sudo /bin/cp -f /tmp/Logs $2
+  
