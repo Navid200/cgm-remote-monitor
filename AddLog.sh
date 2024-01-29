0364f8d76129c83507131f85f710a9df0068781a
Navid <51497406+Navid200@users.noreply.github.com>
2024-01-29 15:30:33 -0500

    Update AddLog.sh

--- AddLog.sh
+++ AddLog.sh
@@ -16,21 +16,22 @@ then
 fi
 
 sudo rm -rf /tmp/Logs
-if [ $? != 0 ]
+if [ $? -ne 0 ]
 then
   echo "Error: Failed to delete temp log file"
   exit 5
 fi
 
 echo -e "$1     $(date)\n" | cat - $2 > /tmp/Logs
-if [ $? != 0 ]
+echo "$?"
+if [ $? -ne 0 ]
 then
   echo "Error: Failed to create temp log file"
   exit 5
 fi
 
 sudo /bin/cp -f /tmp/Logs $2
-if [ $? != 0 ]
+if [ $? -ne 0 ]
 then
   echo "Error: Failed to update logs"
   exit 5
