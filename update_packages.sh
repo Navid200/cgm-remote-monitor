7ca50407357d677009d2efad511a1e6331ba500e
Navid <51497406+Navid200@users.noreply.github.com>
2024-07-31 22:11:26 -0400

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -61,7 +61,7 @@ fi
 
 # node
 whichpack=$(node -v)
-if [ ! "${whichpack%%.*}" = "v14" ]
+if [ ! "${whichpack%%.*}" = "v16" ]
 then
   curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash - &&\
   sudo apt-get install -y nodejs
