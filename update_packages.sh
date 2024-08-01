fc6a5d92ae1e29e4fb97a7694c18ad501d53f218
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-01 19:24:38 -0400

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -54,9 +54,13 @@ fi
 
 # mongo
 whichpack="$(mongod --version | sed -n 1p)"
-if [ ! "${whichpack%%.*}" = "db version v3" ]
+if [ ! "${whichpack%%.*}" = "db version v6" ]
 then
-  sudo apt-get -y install mongodb-server
+  sudo apt-get install gnupg curl
+  curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
+  echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+  sudo apt-get update
+  sudo apt-get install -y mongodb-org=6.0.16 mongodb-org-database=6.0.16 mongodb-org-server=6.0.16 mongodb-org-mongos=6.0.16 mongodb-org-tools=6.0.16
 fi
 
 # node
