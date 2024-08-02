4c585e3093ffe8269c216e61a7fd2b1c1dd73654
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-02 07:19:52 -0400

    Create NS_Install_a.sh

---
+++ NS_Install_a.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo
+echo "JamOrHam Nightscout Installer - Designed for Google Compute Minimal Ubuntu 20 micro instance"
+echo
+
+if [ "`id -u`" != "0" ]
+then
+echo "Script needs root - use sudo bash NS_Install.sh"
+echo "Cannot continue.."
+exit 5
+fi
+
+clear
+dialog --colors --msgbox "      \Zr Developed by the xDrip team \Zn\n\n\
+Some required packages will be installed now.  It will take about 15 minutes to complete.  This terminal needs to be kept open.  Press enter to proceed.\n\n\
+If this is not a good time, you can press escape now to cancel." 13 50
+if [ $? = 255 ]
+then
+clear
+exit
+fi
+clear
+
+if [ ! -s /var/SWAP ]
+then
+echo "Creating swap file"
+dd if=/dev/zero of=/var/SWAP bs=1M count=2000
+chmod 600 /var/SWAP
+mkswap /var/SWAP
+fi
+swapon 2>/dev/null /var/SWAP
+
+echo "Installing system basics"
+sudo apt-get update
+sudo apt-get -y install wget gnupg libcurl4 openssl liblzma5
+sudo apt-get -y install dirmngr apt-transport-https lsb-release ca-certificates
+sudo apt-get -y install net-tools
+sudo apt-get -y install build-essential
+# Please don't add any more utilities here.  Please instead, add them to update_packages.sh.
+
+/xDrip/scripts/update_packages.sh
