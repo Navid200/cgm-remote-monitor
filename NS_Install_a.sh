abef44bd13887195aa47c334e171a0aff04d3932
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-05 09:11:33 -0400

    Update NS_Install_a.sh

--- NS_Install_a.sh
+++ NS_Install_a.sh
@@ -14,12 +14,4 @@ sudo mkswap /var/SWAP
 fi
 sudo swapon 2>/dev/null /var/SWAP
 
-echo "Installing system basics"
-sudo apt-get update
-sudo apt-get -y install wget gnupg libcurl4 openssl liblzma5
-sudo apt-get -y install dirmngr apt-transport-https lsb-release ca-certificates
-sudo apt-get -y install net-tools
-sudo apt-get -y install build-essential
-# Please don't add any more utilities here.  Please instead, add them to update_packages.sh.
 
-/xDrip/scripts/update_packages.sh
