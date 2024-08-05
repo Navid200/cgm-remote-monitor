29a6309f4a8991243d0b932894b476b000c4011e
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-05 16:06:05 -0400

    Create NS_Install_e.sh

---
+++ NS_Install_e.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo
+echo "JamOrHam Nightscout Installer - Designed for Google Compute Minimal Ubuntu 20 micro instance"
+echo
+
+if [ "`id -u`" != "0" ]
+then
+echo "Script needs root - use sudo bash NS_Install.sh"
+echo "Cannot continue.."
+exit 5
+fi
+
+clear
+dialog --colors --msgbox "      \Zr Developed by the xDrip team \Zn\n\n\
+Some required packages will be installed now.  It will take about 15 minutes to complete.  This terminal needs to be kept open.  Press enter to proceed.\n\n\
+If this is not a good time, you can press escape now to cancel." 13 50
+if [ $? = 255 ]
+then
+clear
+exit
+fi
+clear
+
+
+cd /srv
+
+echo "Installing Nightscout"
+cd "$(< repo)" 
+sudo git reset --hard  # delete any local edits.
+sudo git pull  # Update database from remote.
+
+sudo npm install
+sudo npm run postinstall
+sudo npm run-script post-generate-keys
+
+for loop in 1 2 3 4 5 6 7 8 9
+do
+read -t 0.1 dummy
+done
+
+# Add log
+/xDrip/scripts/AddLog.sh "Installation phase 1 completed" /xDrip/Logs
+  
