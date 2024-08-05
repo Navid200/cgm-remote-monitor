ab1ab44dcfbc4c170f4681686234ceab2566867d
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-05 16:50:39 -0400

    Update nodesource_setup.sh

--- nodesource_setup.sh
+++ nodesource_setup.sh
@@ -131,8 +131,7 @@ h sudo"
     rm -f /etc/apt/sources.list.d/nodesource.list || true
 
     # Run 'curl' and 'gpg'
-    if ! curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /usr/share/keyr
-ings/nodesource.gpg; then
+    if ! curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /usr/share/keyrings/nodesource.gpg; then
       handle_error "$?" "Failed to download and import the NodeSource signing key"
     fi
 }
@@ -147,8 +146,7 @@ configure_repo() {
       handle_error "1" "Unsupported architecture: $arch. Only amd64, arm64, and armhf are supported."
     fi
 
-    echo "deb [arch=$arch signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$node_ve
-rsion nodistro main" | tee /etc/apt/sources.list.d/nodesource.list > /dev/null
+    echo "deb [arch=$arch signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$node_version nodistro main" | tee /etc/apt/sources.list.d/nodesource.list > /dev/null
 
     # Nodejs Config
     echo "Package: nodejs" | tee /etc/apt/preferences.d/nodejs > /dev/null
