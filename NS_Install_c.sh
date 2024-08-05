25f6a5feb5c1c0be9eacc8a4d0150f7a08a19486
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-05 16:08:48 -0400

    Update NS_Install_c.sh

--- NS_Install_c.sh
+++ NS_Install_c.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo
-echo "JamOrHam Nightscout Installer - Designed for Google Compute Minimal Ubuntu 20 micro instance"
-echo
-
-
-# Create mongo user and admin.
-echo -e "use Nightscout\ndb.createUser({user: \"username\", pwd: \"password\", roles:[\"readWrite\"]})\nquit()" | mongosh
-echo -e "use admin\ndb.createUser({ user: \"mongoadmin\" , pwd: \"mongoadmin\", roles: [\"userAdminAnyDatabase\", \"dbAdminAnyDatabase\", \"readWriteAnyDatabase\"]})\nquit()" | mongosh
-
+/xDrip/scripts/update_packages.sh
 
   
