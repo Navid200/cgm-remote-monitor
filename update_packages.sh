7e7c9cd347761c8df081ed25e9d351cdcf627d06
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-05 09:48:03 -0400

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -78,7 +78,9 @@ if [ ! "${whichpack%%.*}" = "v16" ]
 then
 #  curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
 #  sudo apt-get install -y nodejs  
-
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo npm install -g npm@latest
 
 fi 
 
