ed7bd0e11be73959985dc71344dcb5ef30990067
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-08 21:39:46 -0400

    Update update_packages2.sh

--- update_packages2.sh
+++ update_packages2.sh
@@ -11,25 +11,25 @@ echo
 # Let's continue upgrading packages if available and install the missing needed packages.
 
 # mongo
-whichpack="$(mongod --version | sed -n 1p)"
-if [ ! "${whichpack%%.*}" = "db version v6" ]
-then
- curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
- echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
- sudo apt-get update
- sudo apt-get install -y mongodb-org=6.0.16 mongodb-org-database=6.0.16 mongodb-org-server=6.0.16 mongodb-org-mongos=6.0.16 mongodb-org-tools=6.0.16
+#whichpack="$(mongod --version | sed -n 1p)"
+#if [ ! "${whichpack%%.*}" = "db version v6" ]
+#then
+# curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
+# echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+# sudo apt-get update
+# sudo apt-get install -y mongodb-org=6.0.16 mongodb-org-database=6.0.16 mongodb-org-server=6.0.16 mongodb-org-mongos=6.0.16 mongodb-org-tools=6.0.16
 
- echo "mongodb-org hold" | sudo dpkg --set-selections
- echo "mongodb-org-database hold" | sudo dpkg --set-selections
- echo "mongodb-org-server hold" | sudo dpkg --set-selections
- echo "mongodb-mongosh hold" | sudo dpkg --set-selections
- echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
- echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+# echo "mongodb-org hold" | sudo dpkg --set-selections
+# echo "mongodb-org-database hold" | sudo dpkg --set-selections
+# echo "mongodb-org-server hold" | sudo dpkg --set-selections
+# echo "mongodb-mongosh hold" | sudo dpkg --set-selections
+# echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+# echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 
-  sudo systemctl start mongod
+#  sudo systemctl start mongod
   # sudo systemctl status mongod
-  sudo systemctl enable mongod
-fi
+#  sudo systemctl enable mongod
+#fi
 
 # node - We install version 16 of node here, which automatically  updates npm to 8.
 whichpack=$(node -v)
