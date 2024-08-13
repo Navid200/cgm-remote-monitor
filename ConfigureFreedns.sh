11112a607043cf96fcc3bba434da4f6605b1ff3b
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-13 08:46:08 -0400

    Update ConfigureFreedns.sh

--- ConfigureFreedns.sh
+++ ConfigureFreedns.sh
@@ -182,7 +182,7 @@ while : ; do
     fi
     sudo resolvectl flush-caches
     ping -c 1 $hostname
-    sudo resolvectl -4
+    sudo resolvectl -4 $hostname
     if [ $cnt -gt 20 ]
     then
       clear
