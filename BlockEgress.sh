c27343dcb4fae65f4c746fe370539ca22dd07d30
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-21 10:04:57 -0400

    Update BlockEgress.sh

--- BlockEgress.sh
+++ BlockEgress.sh
@@ -17,14 +17,10 @@ sudo apt-get -y install iptables-persistent
  
 if ! grep -q "net.ipv6.conf.all.disable_ipv6 = 1" /etc/sysctl.conf
 then
-#  rm /tmp/sysctl.conf
- # cp /etc/sysctl.conf /tmp/sysctl.conf
-#  echo "net.ipv6.conf.all.disable_ipv6 = 1\nnet.ipv6.conf.default.disable_ipv6 = 1\nnet.ipv6.conf.lo.disable_ipv6 = 1" >> /tmp/sysctl.conf
   sudo cat << EOT >> /etc/sysctl.conf
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
 EOT
 
-#  sudo mv /tmp/sysctl.conf /etc/sysctl.conf
 fi
