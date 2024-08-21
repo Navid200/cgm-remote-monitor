4555497a262fc76a7b8d880943fa3c4faaf43310
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-21 08:09:44 -0400

    Create BlockEgress.sh

---
+++ BlockEgress.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo
+echo "Blocking China and/or Australia - Navid200"
+echo
+
+# Google charges outgoing traffic greater than 1GB per month.
+# However, the 1GB limit does not apply to China and Australia.
+# Any outgoing traffic to China and Australia even if less than 1GB will be charged.
+# This utility allows us to block traffic to China and or Australia.
+
+
+
+ 
