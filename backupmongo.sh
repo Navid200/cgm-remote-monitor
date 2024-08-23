757b4f21b7fb44db9f8c2b80d704692431cf2a11
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-22 23:41:17 -0400

    Update backupmongo.sh

--- backupmongo.sh
+++ backupmongo.sh
@@ -41,10 +41,13 @@ echo""
 readlink -f $Filename
 echo""
 echo""
-exit
-fi
-done
+
 
 dialog --colors --msgbox "       \Zr Developed by the xDrip team \Zn\n\n\n\
 Backup is complete.\n\
 However, it is on the same virtual machine that your database and variables are on.  It's best to download the file to your computer for safekeeping." 13 50
+
+
+exit
+fi
+done
