934c22476cd1180915dbdb796dffe39bf9c8179d
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-23 00:31:20 -0400

    Update backupmongo.sh

--- backupmongo.sh
+++ backupmongo.sh
@@ -29,9 +29,6 @@ cd /tmp
 cp /etc/nsconfig .
 tar -cf ~/$Filename database.gz nsconfig
 
-fi
-done
-
 dialog --colors --msgbox "       \Zr Developed by the xDrip team \Zn\n\n\n\
 Backup is complete.\n\
 However, it is on the same virtual machine that your database and variables are on.  It's best to download the file to your computer for safekeeping." 13 50
@@ -45,6 +42,7 @@ readlink -f $Filename
 echo""
 echo""
 read -p "Press any key to continue... " -n1 -s 
-
-/xDrip/scripts/menu.sh
+ 
+fi
+done
  
