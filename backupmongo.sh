c3b335a8441fc1fa07ce01257ee9319453ed4e73
Navid <51497406+Navid200@users.noreply.github.com>
2024-08-24 09:56:46 -0400

    Update backupmongo.sh

--- backupmongo.sh
+++ backupmongo.sh
@@ -31,7 +31,7 @@ tar -cf ~/$Filename database.gz nsconfig
 
  dialog --colors --infobox "       \Zr Developed by the xDrip team \Zn\n\n\n\
 Backup is complete.\n\
-However, it is stored on the virtual machine.  It's best to download it to your computer for safekeeping.\n\nCopy the full path to the backup file shown below, and click on \"DOWNLOAD FILE\" above to download.\n\nPress any key to exit." 13 50
+However, it is stored on the virtual machine.  It's best to download it to your computer for safekeeping.\n\nCopy the full path to the backup file shown below, and click on \"DOWNLOAD FILE\" above to download it.\n\nPress any key to exit." 15 50
 cd ~ 
 echo ""
 echo ""
