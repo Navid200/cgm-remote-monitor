32addd02e8f18062081c09b40ba65492844fe0d9
Navid <51497406+Navid200@users.noreply.github.com>
2024-09-09 20:44:45 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,7 +45,7 @@ The Ubuntu version on the virtual machine is incorrect.  You need to delete the 
   fi 
 
   sudo apt-get install -y  git python gcc g++ make
-  sudo apt-get -y install netcat
+  sudo apt-get -y install netcat-traditional
 fi
 
 if [ ! -s /xDrip ]
