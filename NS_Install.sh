791ce6c736b08bfe8a04ada895af417dbbff4c11
Navid <51497406+Navid200@users.noreply.github.com>
2024-09-09 23:21:11 -0400

    Update NS_Install.sh

--- NS_Install.sh
+++ NS_Install.sh
@@ -38,7 +38,7 @@ sudo apt-get update
 
 # Create mongo user and admin.
 echo -e "use Nightscout\ndb.createUser({user: \"username\", pwd: \"password\", roles:[\"readWrite\"]})\nquit()" | mongosh
-echo -e "use admin\ndb.createUser({ user: \"mongoadmin\" , pwd: \"mongoadmin\", roles: [\"userAdminAnyDatabase\", \"dbAdminAnyDatabase\", \"readWriteAnyDatabase\"]})\nquit()" | mongo
+echo -e "use admin\ndb.createUser({ user: \"mongoadmin\" , pwd: \"mongoadmin\", roles: [\"userAdminAnyDatabase\", \"dbAdminAnyDatabase\", \"readWriteAnyDatabase\"]})\nquit()" | mongosh
 
 cd /srv
 
