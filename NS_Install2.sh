ec8a907dac9b1f6431e516d544361abac4c6db20
Navid <51497406+Navid200@users.noreply.github.com>
2024-11-12 17:27:57 -0500

    Update NS_Install2.sh

--- NS_Install2.sh
+++ NS_Install2.sh
@@ -177,7 +177,8 @@ dialog --colors --msgbox "       \Zr Developed by the xDrip team \Zn\n\n\
 Press enter to restart the server.  This will result in an expected error message.  Wait 30 seconds before clicking on retry to reconnect or using a browser to access your Nightscout." 10 50
 clear
 sudo reboot
-dialog --colors --pause "       \Zr Developed by the xDrip team \Zn\n\n\
-Please be patient for the system to reboot in 30 seconds." 11 50 30
+dialog --colors --infobox "       \Zr Developed by the xDrip team \Zn\n\n\
+Please be patient for the system to reboot in 30 seconds." 11 50
+exit
 fi
  
