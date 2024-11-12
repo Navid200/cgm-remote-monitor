bd9d812af6ea02290eb6c67c6e7b2379f3111a8f
Navid <51497406+Navid200@users.noreply.github.com>
2024-11-12 17:33:14 -0500

    Update Status.sh

--- Status.sh
+++ Status.sh
@@ -66,7 +66,7 @@ fi
 
 mongo="$(mongod --version | awk '/db version/ {print $NF}')"
 
-ns="$(ps -ef | grep SCREEN | grep root | fold --width=40 | sed -n 1p)"
+ns="$(ps -ef | grep SCREEN | grep root | fold --width=34 | sed -n 1p)"
 
 uname="$(< /srv/username)"
 if [ ! "$(< /srv/username)" = "jamorham" ]
