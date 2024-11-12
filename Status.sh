9b71c685bf8ba41d5e8fb53d92883600482fc343
Navid <51497406+Navid200@users.noreply.github.com>
2024-11-12 17:11:00 -0500

    Update Status.sh

--- Status.sh
+++ Status.sh
@@ -64,7 +64,8 @@ then
 http="\Zb\Z1Closed\Zn" # Set color to red if the Firewall is not set.
 fi
 
-mongo="$(mongod --version | sed -n 1p)"
+mongo="$(mongod --version | awk '/db version/ {print $NF}')"
+
 ns="$(ps -ef | grep SCREEN | grep root | fold --width=40 | sed -n 1p)"
 
 uname="$(< /srv/username)"
