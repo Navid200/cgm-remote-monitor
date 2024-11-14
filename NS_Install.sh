02673a6f527315bed2642d5f69ae07f6201ba9a8
Navid <51497406+Navid200@users.noreply.github.com>
2024-11-13 22:37:35 -0500

    Update NS_Install.sh

--- NS_Install.sh
+++ NS_Install.sh
@@ -31,8 +31,27 @@ mkswap /var/SWAP
 fi
 swapon 2>/dev/null /var/SWAP
 
+# mongo
+whichpack="$(mongod --version | sed -n 1p)"
+if [ ! "${whichpack%%.*}" = "db version v6" ]
+then
+ curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
+ echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+ sudo apt-get update
+ sudo apt-get install -y mongodb-org=6.0.16 mongodb-org-database=6.0.16 mongodb-org-server=6.0.16 mongodb-org-mongos=6.0.16 mongodb-org-tools=6.0.16
+
+ echo "mongodb-org hold" | sudo dpkg --set-selections
+ echo "mongodb-org-database hold" | sudo dpkg --set-selections
+ echo "mongodb-org-server hold" | sudo dpkg --set-selections
+ echo "mongodb-mongosh hold" | sudo dpkg --set-selections
+ echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+ echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+  sudo systemctl start mongod
+  sudo systemctl enable mongod
+fi
+
 # Please don't add any utility installs here.  Please instead, add them to update_packages.sh.
-/xDrip/scripts/update_packages.sh
 
 sudo apt-get update
 
