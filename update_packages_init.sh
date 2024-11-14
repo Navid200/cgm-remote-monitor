dc04ecc0d9e5550ce961bb492d823fbadeaad2c3
Navid <51497406+Navid200@users.noreply.github.com>
2024-11-14 09:42:14 -0500

    Update update_packages_init.sh

--- update_packages_init.sh
+++ update_packages_init.sh
@@ -8,18 +8,17 @@ echo
 sudo apt-get update
 
 # packages
-whichpack=$(which file)
-if [ "$whichpack" = "" ]
-then
-  sudo apt-get -y install liblzma5 
-fi
+#whichpack=$(which file)
+#if [ "$whichpack" = "" ]
+#then
+#  sudo apt-get -y install liblzma5 
+#fi
 
 # node - We install version 16 of node here, which automatically  updates npm to 8.
 whichpack=$(node -v)
 if [ ! "${whichpack%%.*}" = "v16" ]
 then
 sudo /xDrip/scripts/nodesource_setup.sh
-# sudo apt install -y nodejs
 sudo apt-get install nodejs -y
 # Nightscout needs version 6 of npm.  So, we are going to install that version now effectivwely downgrading it.  
 sudo npm install -g npm@6.14.18
