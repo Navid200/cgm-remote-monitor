5ca5b2b1b078ebacbbcee153a55b9a12a86ea569
Navid <51497406+Navid200@users.noreply.github.com>
2024-11-13 22:39:10 -0500

    Update NS_Install.sh

--- NS_Install.sh
+++ NS_Install.sh
@@ -51,6 +51,17 @@ then
   sudo systemctl enable mongod
 fi
 
+# node - We install version 16 of node here, which automatically  updates npm to 8.
+whichpack=$(node -v)
+if [ ! "${whichpack%%.*}" = "v16" ]
+then
+sudo /xDrip/scripts/nodesource_setup.sh
+# sudo apt install -y nodejs
+sudo apt-get install nodejs -y
+# Nightscout needs version 6 of npm.  So, we are going to install that version now effectivwely downgrading it.  
+sudo npm install -g npm@6.14.18
+fi
+
 # Please don't add any utility installs here.  Please instead, add them to update_packages.sh.
 
 sudo apt-get update
