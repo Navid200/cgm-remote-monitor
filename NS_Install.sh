f70000ecfa9c281ccd6d7943cde70169be9fd938
Navid <51497406+Navid200@users.noreply.github.com>
2024-11-14 22:29:53 -0500

    Update NS_Install.sh

--- NS_Install.sh
+++ NS_Install.sh
@@ -31,16 +31,20 @@ mkswap /var/SWAP
 fi
 swapon 2>/dev/null /var/SWAP
 
-sudo apt-get update
+apt-get update
+if dpkg -s build-essential &>/dev/null
+then
+  apt-get -y build-essential
+fi
 
 # node - We install version 16 of node here, which automatically  updates npm to 8.
 whichpack=$(node -v)
 if [ ! "${whichpack%%.*}" = "v16" ]
 then
-  sudo /xDrip/scripts/nodesource_setup.sh
-  sudo apt-get install nodejs -y
+  /xDrip/scripts/nodesource_setup.sh
+  apt-get install nodejs -y
   # Nightscout needs version 6 of npm.  So, we are going to install that version now effectivwely downgrading it.  
-  sudo npm install -g npm@6.14.18
+  npm install -g npm@6.14.18
 fi
 
 cd /srv
