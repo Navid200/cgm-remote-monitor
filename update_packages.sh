c9bc23fc5ea47bc9863632289077d5199d691f99
Navid <51497406+Navid200@users.noreply.github.com>
2024-11-15 08:33:28 -0500

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -21,7 +21,7 @@ sudo apt-get update
 whichpack=$(which file)
 if [ "$whichpack" = "" ]
 then
-  sudo apt-get -y install vis nano screen jq qrencode file net-tools gnupg liblzma5 apt-transport-https lsb-release ca-certificates build-essential
+  sudo apt-get -y install vis nano screen jq qrencode file net-tools gnupg liblzma5 apt-transport-https lsb-release ca-certificates nginx python3-certbot-nginx inetutils-ping
 fi
 
 # mongo
