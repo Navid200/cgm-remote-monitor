48f8caa7485450cad39fb174a657355d16f696c7
Navid <51497406+Navid200@users.noreply.github.com>
2024-11-17 21:50:27 -0500

    Create update_packages2.sh

---
+++ update_packages2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo
+echo "Install packages after the installation of Nightscout. - Navid200"
+echo
+
+# Let's upgrade packages if available and install the missing needed packages.
+sudo apt-get update
+
+# packages
+whichpack=$(which file)
+if [ "$whichpack" = "" ]
+then
+  sudo apt-get -y install nano qrencode file gnupg
+fi 
+
+# mongo
+whichpack="$(mongod --version | sed -n 1p)"
+if [ ! "${whichpack%%.*}" = "db version v6" ]
+then
+ curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
+ echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+ sudo apt-get update
+ sudo apt-get install -y mongodb-org=6.0.16 mongodb-org-database=6.0.16 mongodb-org-server=6.0.16 mongodb-org-mongos=6.0.16 mongodb-org-tools=6.0.16
+
+ echo "mongodb-org hold" | sudo dpkg --set-selections
+ echo "mongodb-org-database hold" | sudo dpkg --set-selections
+ echo "mongodb-org-server hold" | sudo dpkg --set-selections
+ echo "mongodb-mongosh hold" | sudo dpkg --set-selections
+ echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+ echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+  sudo systemctl start mongod
+  sudo systemctl enable mongod
+fi
+# Create mongo user and admin.
+echo -e "use Nightscout\ndb.createUser({user: \"username\", pwd: \"password\", roles:[\"readWrite\"]})\nquit()" | mongosh
+echo -e "use admin\ndb.createUser({ user: \"mongoadmin\" , pwd: \"mongoadmin\", roles: [\"userAdminAnyDatabase\", \"dbAdminAnyDatabase\", \"readWriteAnyDatabase\"]})\nquit()" | mongosh
+
+# The last item on the above list of packages must be verified in Status.sh to have been installed.  
+
+# Add log
+/xDrip/scripts/AddLog.sh "The secondary packages have been installed" /xDrip/Logs
+  
