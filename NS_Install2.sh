eca850ed241102731db5d893fc2fe5ec09037383
Navid <51497406+Navid200@users.noreply.github.com>
2024-11-17 20:13:27 -0500

    Update NS_Install2.sh

--- NS_Install2.sh
+++ NS_Install2.sh
@@ -19,6 +19,28 @@ echo "Cannot continue.."
 exit 5
 fi
 
+sudo apt-get update
+
+# mongo
+whichpack="$(mongod --version | sed -n 1p)"
+if [ ! "${whichpack%%.*}" = "db version v6" ]
+then
+ curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor
+ echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+ sudo apt-get update
+ sudo apt-get install -y mongodb-org=6.0.16 mongodb-org-database=6.0.16 mongodb-org-server=6.0.16 mongodb-org-mongos=6.0.16 mongodb-org-tools=6.0.16
+
+ echo "mongodb-org hold" | sudo dpkg --set-selections
+ echo "mongodb-org-database hold" | sudo dpkg --set-selections
+ echo "mongodb-org-server hold" | sudo dpkg --set-selections
+ echo "mongodb-mongosh hold" | sudo dpkg --set-selections
+ echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+ echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+
+  sudo systemctl start mongod
+  sudo systemctl enable mongod
+fi
+
 sudo apt-get install -y nginx python3-certbot-nginx inetutils-ping
 
 if [ "`grep '.well-known' /etc/nginx/sites-enabled/default`" = "" ]
