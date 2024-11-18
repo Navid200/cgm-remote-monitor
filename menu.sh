0437841aa15c3ee8a7713d78a1fdf2dcdb7be0ec
Navid <51497406+Navid200@users.noreply.github.com>
2024-11-18 17:02:22 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -62,7 +62,7 @@ else
 clear
 sudo reboot
 dialog --colors --pause "       \Zr Developed by the xDrip team \Zn\n\n\
-Please wait 30 seconds for the system to reboot. An expected error message will then appear. Wait an additional 30 seconds before attempting to reconnect." 14 50 30
+Please wait 25 seconds for the system to reboot. An expected error message will then appear. Wait an additional 30 seconds before attempting to reconnect." 14 50 30
 exit
 fi
 ;;
