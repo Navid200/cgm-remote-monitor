8e48a177f609ca6b2446479746f0de2cb57f4986
Navid <51497406+Navid200@users.noreply.github.com>
2024-11-17 22:38:38 -0500

    Update update_packages2.sh

--- update_packages2.sh
+++ update_packages2.sh
@@ -11,7 +11,7 @@ sudo apt-get update
 whichpack=$(which file)
 if [ "$whichpack" = "" ]
 then
-  sudo apt-get -y install nano qrencode file gnupg
+  sudo apt-get -y install nano qrencode file
 fi 
 
 # mongo
