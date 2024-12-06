563b905eb5d9505d91fadfe49e3ebe669f92166d
Navid <51497406+Navid200@users.noreply.github.com>
2024-12-05 20:19:18 -0500

    Update menu_GC_Setup.sh

--- menu_GC_Setup.sh
+++ menu_GC_Setup.sh
@@ -11,7 +11,7 @@ Use the arrow keys to move the cursor.\n\
 Press Enter to execute the highlighted option.\n" 17 50 7\
  "1" "Install Nightscout phase 1 - 15 minutes"\
  "2" "Install Nightscout phase 2 - 5 minutes"\
- "3" "Update platform"\
+ "3" "Update"\
  "4" "Return"\
  3>&1 1>&2 2>&3)
 
@@ -28,20 +28,27 @@ sudo /xDrip/scripts/NS_Install2.sh
 ;;
 
 3)
-cd /srv
-cd "$(< repo)"  # Go to the local database
-sudo git reset --hard  # delete any local edits.
-sudo git pull  # Update database from remote.
-sudo chmod 755 update_scripts.sh
-sudo cp -f update_scripts.sh /xDrip/scripts/. # Update the "update scripts" script. 
-clear
-/xDrip/scripts/update_scripts.sh
-/xDrip/scripts/update_packages.sh
-/xDrip/scripts/update_packages2.sh
-/xDrip/scripts/StartUpSetup.sh
-clear
-dialog --colors --msgbox "        \Zr Developed by the xDrip team \Zn\n\n\
+dialog --colors --yesno "       \Zr Developed by the xDrip team \Zn\n\n\
+This will update your server and Nighsctout to the latest available updates.  Press enter to proceed or ESC to cancel."  9 50
+response=$?
+if [ $response = 255 ] || [ $response = 1 ]
+then
+  clear
+else
+  cd /srv
+  cd "$(< repo)"  # Go to the local database
+  sudo git reset --hard  # delete any local edits.
+  sudo git pull  # Update database from remote.
+  sudo chmod 755 update_scripts.sh
+  sudo cp -f update_scripts.sh /xDrip/scripts/. # Update the "update scripts" script. 
+  /xDrip/scripts/update_scripts.sh
+  /xDrip/scripts/update_packages.sh
+  /xDrip/scripts/update_packages2.sh
+  /xDrip/scripts/StartUpSetup.sh
+  clear
+  dialog --colors --msgbox "        \Zr Developed by the xDrip team \Zn\n\n\
 Close this terminal to complete updates." 7 50
+fi
 ;;
 
 4)
