4b3d497e9977f024a9b80abcca675a627b796027
Navid <51497406+Navid200@users.noreply.github.com>
2024-12-06 09:22:03 -0500

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -21,7 +21,7 @@ sudo apt-get update
 whichpack=$(which gpg)
 if [ "$whichpack" = "" ]
 then
-  sudo apt-get -y install vis jq net-tools apt-transport-https lsb-release ca-certificates gnupg
+  sudo apt-get -y install jq net-tools apt-transport-https ca-certificates gnupg
 fi 
 
 # node - We install version 16 of node here, which automatically  updates npm to 8.
