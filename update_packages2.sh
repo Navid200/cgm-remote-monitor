71b84ea774efed3bc626d0774ae3d6977a3f4f67
Navid <51497406+Navid200@users.noreply.github.com>
2024-12-06 21:30:59 -0500

    Update update_packages2.sh

--- update_packages2.sh
+++ update_packages2.sh
@@ -12,6 +12,8 @@ whichpack=$(which file)
 if [ "$whichpack" = "" ]
 then
   sudo apt-get -y install screen nano qrencode file vis lsb-release apt-transport-https ca-certificates 
+  sudo apt-get -y install python3-pip
+  sudo apt -y install python3-django python3-django-extensions python3-werkzeug python3-qrcode
 fi 
 
 # The last item on the above list of packages must be verified in Status.sh to have been installed.  
