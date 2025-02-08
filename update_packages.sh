2c0e5b442bfa412aa488ee01f4787ea5754848a2
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-08 17:50:43 -0500

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -50,14 +50,12 @@ then
   echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
   echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 
-  /xDrip/scripts/AddLog.sh "Starting mongod" /xDrip/Logs
   sudo systemctl start mongod
-  sleep 5
   sudo systemctl enable mongod
+  sleep 10
 
   # Create mongo user and admin.
   echo -e "use Nightscout\ndb.createUser({user: \"username\", pwd: \"password\", roles:[\"readWrite\"]})\nquit()" | mongosh
-  sleep 5
   echo -e "use admin\ndb.createUser({ user: \"mongoadmin\" , pwd: \"mongoadmin\", roles: [\"userAdminAnyDatabase\", \"dbAdminAnyDatabase\", \"readWriteAnyDatabase\"]})\nquit()" | mongosh
 
 fi
