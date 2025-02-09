d23db7b00e089d22710890e7c27a1644a3dbb574
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-08 19:50:16 -0500

    Update NS_Install.sh

--- NS_Install.sh
+++ NS_Install.sh
@@ -36,6 +36,10 @@ swapon 2>/dev/null /var/SWAP
 
 apt-get update
 
+# Create mongo user and admin.
+echo -e "use Nightscout\ndb.createUser({user: \"username\", pwd: \"password\", roles:[\"readWrite\"]})\nquit()" | mongosh
+echo -e "use admin\ndb.createUser({ user: \"mongoadmin\" , pwd: \"mongoadmin\", roles: [\"userAdminAnyDatabase\", \"dbAdminAnyDatabase\", \"readWriteAnyDatabase\"]})\nquit()" | mongosh
+
 cd /srv
 
 echo "Installing Nightscout"
