51d1d6f098d88ff9479957a3bbcf035402e7fe56
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-10 19:38:49 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -59,11 +59,7 @@ if [ $response = 255 ] || [ $response = 1 ]
 then
 clear
 else
-clear
-sudo reboot
-dialog --colors --no-cancel --ok-label "Please wait" --pause "       \Zr Developed by the xDrip team \Zn\n\n\
-Please wait 25 seconds for the system to reboot. An expected error message will then appear. Wait an additional 30 seconds before attempting to reconnect." 14 50 25
-exit
+/xDrip/scripts/reboot.sh
 fi
 ;;
 
