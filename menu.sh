7da962542321bad840eebcd3d63c3434d0201aad
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-11 13:57:57 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -67,19 +67,20 @@ fi
 8)
 clear
 dialog --colors --infobox "       \Zr Developed by the xDrip team \Zn\n\n\
-        Support\n\
-Press any key to return to the main menu." 6 50
+               Support\n\n\n\
+Click on links above to access corresponding support pages.\n\
+Press any key to return to the main menu." 10 50
 
 #Copy the hyperlink below without using CTRL+C and paste it into your web browser.\n\n\
 
 tput civis
-printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Tips.html\e\\Tips\e]8;;\e\\\n'
+printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Tips.html\e\\Tips   \e]8;;\e\\\n'
 echo ""
-printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/FAQ.html\e\\FAQ\e]8;;\e\\\n'
+printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/FAQ.html\e\\FAQ   \e]8;;\e\\\n'
 echo ""
-printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Troubleshooting.html\e\\Troubleshooting\e]8;;\e\\\n'
+printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Troubleshooting.html\e\\Troubleshooting   \e]8;;\e\\\n'
 echo ""
-printf '>  \e]8;;https://github.com/NightscoutFoundation/xDrip/discussions\e\\Contact us\e]8;;\e\\\n\n\n'
+printf '>  \e]8;;https://github.com/NightscoutFoundation/xDrip/discussions\e\\Contact us   \e]8;;\e\\\n\n\n'
 echo ""
 read -p "" -n1 -s
 tput cnorm
