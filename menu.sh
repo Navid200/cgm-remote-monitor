e8a3312ce8f8b6c085556b6274c28959948834c2
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-11 14:17:59 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -67,22 +67,22 @@ fi
 8)
 clear
 dialog --colors --infobox "       \Zr Developed by the xDrip team \Zn\n\n\
-                 SUPPORT\n\n\n\
+                 SUPPORT\n\n\
 Click on links above to access corresponding support pages.\n\n\n\
 Press any key to return to the main menu." 12 50
 
 #Copy the hyperlink below without using CTRL+C and paste it into your web browser.\n\n\
 
 tput civis
-printf '                      \n'
+printf '                     \n'
 printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Tips.html\e\\Tips              \e]8;;\e\\\n'
-printf '                      \n'
+printf '                     \n'
 printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/FAQ.html\e\\FAQ               \e]8;;\e\\\n'
-printf '                      \n'
+printf '                     \n'
 printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Troubleshooting.html\e\\Troubleshooting   \e]8;;\e\\\n'
-printf '                      \n'
+printf '                     \n'
 printf '>  \e]8;;https://github.com/NightscoutFoundation/xDrip/discussions\e\\Contact us        \e]8;;\e\\\n'
-printf '            \n\n\n'
+printf '                     \n\n\n'
 read -p "" -n1 -s
 tput cnorm
 
