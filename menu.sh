76a40b32239593a2e2eecadd1c16cbf9f37df45e
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-11 13:46:58 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -67,7 +67,7 @@ fi
 8)
 clear
 dialog --colors --infobox "                      \Zr Developed by the xDrip team \Zn\n\n\n\
-Press any key to return to the main menu." 17 80
+Press any key to return to the main menu." 8 50
 
 #Copy the hyperlink below without using CTRL+C and paste it into your web browser.\n\n\
 
