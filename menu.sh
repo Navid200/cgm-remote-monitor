cfe9088df51aabb44aa634abc5bc573281f7de4d
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-11 20:35:30 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -26,7 +26,7 @@ Press Enter to execute the highlighted option.\n\n" 18 50 8\
  if [ $response = 2 ] # Help
  then
    clear
-   dialog --colors --infobox "Press any key to return to the main menu." 3 48
+   dialog --colors --no-shadow --infobox "Press any key to return to the main menu." 3 48
 
    tput civis
    printf '     Developed by the xDrip team        \n'
@@ -39,8 +39,8 @@ Press Enter to execute the highlighted option.\n\n" 18 50 8\
    printf '                                        \n'
    printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Troubleshooting.html\e\\  Troubleshooting  \e]8;;\e\\                  \n'
    printf '                                        \n'
-   printf '>  \e]8;;https://github.com/NightscoutFoundation/xDrip/discussions\e\\  Contact us       \e]8;;\e\\                   \n'
-   printf '                                         \n'
+   printf '>  \e]8;;https://github.com/NightscoutFoundation/xDrip/discussions\e\\  Contact us       \e]8;;\e\\                  \n'
+   printf '                                        \n'
    read -p "" -n1 -s
    tput cnorm
  fi
