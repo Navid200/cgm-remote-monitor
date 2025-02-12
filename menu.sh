906e7f72e113b64d4b2a9aac85d6c53fe05d07fe
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-11 22:01:10 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -33,16 +33,27 @@ Press Enter to execute the highlighted option.\n\n" 18 50 8\
    printf '\n'
    printf '\e[1;44m%-6s\e[m' " "
    printf '     Developed by the xDrip team       \n'
+   printf '\e[1;44m%-6s\e[m' " "
    printf '                                        \n'
+   printf '\e[1;44m%-6s\e[m' " "
    printf ' Click on the links below to access     \n the corresponding support pages.       \n'
+   printf '\e[1;44m%-6s\e[m' " "
    printf '                                        \n'
+   printf '\e[1;44m%-6s\e[m' " "
    printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Tips.html\e\\  Tips             \e]8;;\e\\                  \n'
+   printf '\e[1;44m%-6s\e[m' " "
    printf '                                        \n'
+   printf '\e[1;44m%-6s\e[m' " "
    printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/FAQ.html\e\\  FAQ              \e]8;;\e\\                  \n'
+   printf '\e[1;44m%-6s\e[m' " "
    printf '                                        \n'
+   printf '\e[1;44m%-6s\e[m' " "
    printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Troubleshooting.html\e\\  Troubleshooting  \e]8;;\e\\                  \n'
+   printf '\e[1;44m%-6s\e[m' " "
    printf '                                        \n'
+   printf '\e[1;44m%-6s\e[m' " "
    printf '>  \e]8;;https://github.com/NightscoutFoundation/xDrip/discussions\e\\  Contact us       \e]8;;\e\\                  \n'
+   printf '\e[1;44m%-6s\e[m' " "
    printf '                                        \n'
    read -p "" -n1 -s
    tput cnorm
