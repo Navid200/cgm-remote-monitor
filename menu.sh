cec523f93a6fcdef8ecd11220e0a90f7cc711011
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-11 22:31:34 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -52,11 +52,11 @@ Press Enter to execute the highlighted option.\n\n" 18 50 8\
    printf '\e[1;44m%-6s\e[m' " "
    printf '                                     \n'
    printf '\e[1;44m%-6s\e[m' " "
-   printf '   \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Troubleshooting.html\e\\ Troubleshooting  \e]8;;\e\\                \n'
+   printf '   \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Troubleshooting.html\e\\ Troubleshooting \e]8;;\e\\                 \n'
    printf '\e[1;44m%-6s\e[m' " "
    printf '                                     \n'
    printf '\e[1;44m%-6s\e[m' " "
-   printf '   \e]8;;https://github.com/NightscoutFoundation/xDrip/discussions\e\\ Contact us  \e]8;;\e\\                     \n'
+   printf '   \e]8;;https://github.com/NightscoutFoundation/xDrip/discussions\e\\ Contact us \e]8;;\e\\                      \n'
    printf '\e[1;44m%-6s\e[m' " "
    printf '                                     \n'
    read -p "" -n1 -s
