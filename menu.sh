5fa4dd2a76e751c53735fba67bc92bff501116b8
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-11 19:42:58 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -11,7 +11,7 @@ clear
 Choice=$(dialog --colors --nocancel --help-button --menu "\
         \Zr Developed by the xDrip team \Zn\n\n
 Use the arrow keys to move the cursor.\n\
-Press Enter to execute the highlighted option.\n\n" 19 50 9\
+Press Enter to execute the highlighted option.\n\n" 18 50 8\
  "1" "Status (may take up to 2 minutes.)"\
  "2" "Logs"\
  "3" "Google Cloud setup"\
@@ -19,8 +19,7 @@ Press Enter to execute the highlighted option.\n\n" 19 50 9\
  "5" "xDrip setup"\
  "6" "Data"\
  "7" "Reboot server (Nightscout)"\
- "8" "Support"\
- "9" "Exit to shell (terminal)"\
+ "8" "Exit to shell (terminal)"\
  3>&1 1>&2 2>&3)
 
  response=$?
@@ -88,37 +87,6 @@ fi
 ;;
 
 8)
-clear
-dialog --colors --infobox "       \Zr Developed by the xDrip team \Zn\n\n\
-                 SUPPORT\n\n\
-Click on links above to access corresponding support pages.\n\n\n\
-Press any key to return to the main menu." 12 50
-
-#Copy the hyperlink below without using CTRL+C and paste it into your web browser.\n\n\
-
-tput civis
-printf '                       \n'
-printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Tips.html\e\\  Tips              \e]8;;\e\\\n'
-printf '                       \n'
-printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/FAQ.html\e\\  FAQ               \e]8;;\e\\\n'
-printf '                       \n'
-printf '>  \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Troubleshooting.html\e\\  Troubleshooting   \e]8;;\e\\\n'
-printf '                       \n'
-printf '>  \e]8;;https://github.com/NightscoutFoundation/xDrip/discussions\e\\  Contact us        \e]8;;\e\\\n'
-printf '                       \n'
-read -p "" -n1 -s
-tput cnorm
-
-#FAQ: \Z1 https://navid200.github.io/xDrip/docs/Nightscout/GCNS/FAQ.html \Zn\n\n\
-#Troubleshooting:  \Z1 https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Troubleshooting.html \Zn\n\n\
-#Contact us: \Z1 https://github.com/NightscoutFoundation/xDrip/discussions \Zn\n\n\n\
-#Press any key to return to the main menu." 17 80
-#tput civis
-#read -p "" -n1 -s
-#tput cnorm
-;;
-
-9)
 cd /tmp
 clear
 dialog --colors --msgbox "        \Zr Developed by the xDrip team \Zn\n\n\
