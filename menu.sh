13acd8a26a95e0b14a30dedfdf967b2f61e8b61c
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-13 10:33:33 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -37,27 +37,27 @@ Click OK or press Enter to select the highlighted option.\n\n" 18 50 8\
    printf '\e[1;44m%-6s\e[m' " "
    printf '                                               \n'
    printf '\e[1;44m%-6s\e[m' " "
-   printf ' Click on the links below to access            \n'
+   printf ' Click on the links below to access the        \n'
    printf '\e[1;44m%-6s\e[m' " "
-   printf ' the corresponding support pages.              \n'
-   printf '\e[1;44m%-6s\e[m' " "
-   printf '                                               \n'
+   printf ' corresponding support pages.              \n'
    printf '\e[1;44m%-6s\e[m' " "
    printf '                                               \n'
    printf '\e[1;44m%-6s\e[m' " "
-   printf '   \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Tips.html\e\\ Tips \e]8;;\e\\                            \n'
+   printf '                                               \n'
+   printf '\e[1;44m%-6s\e[m' " "
+   printf '   \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Tips.html\e\\ Tips \e]8;;\e\\                                      \n'
    printf '\e[1;44m%-6s\e[m' " "
    printf '                                               \n'
    printf '\e[1;44m%-6s\e[m' " "
-   printf '   \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/FAQ.html\e\\ FAQ \e]8;;\e\\                             \n'
+   printf '   \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/FAQ.html\e\\ FAQ \e]8;;\e\\                                       \n'
    printf '\e[1;44m%-6s\e[m' " "
    printf '                                               \n'
    printf '\e[1;44m%-6s\e[m' " "
-   printf '   \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Troubleshooting.html\e\\ Troubleshooting \e]8;;\e\\                 \n'
+   printf '   \e]8;;https://navid200.github.io/xDrip/docs/Nightscout/GCNS/Troubleshooting.html\e\\ Troubleshooting \e]8;;\e\\                           \n'
    printf '\e[1;44m%-6s\e[m' " "
    printf '                                               \n'
    printf '\e[1;44m%-6s\e[m' " "
-   printf '   \e]8;;https://github.com/NightscoutFoundation/xDrip/discussions\e\\ Contact us \e]8;;\e\\                      \n'
+   printf '   \e]8;;https://github.com/NightscoutFoundation/xDrip/discussions\e\\ Contact us \e]8;;\e\\                                \n'
    printf '\e[1;44m%-6s\e[m' " "
    printf '                                               \n'
    read -p "" -n1 -s
