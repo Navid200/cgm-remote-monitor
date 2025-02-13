b55d05991afa9c700914dcad22bc4e563358fe0f
Navid <51497406+Navid200@users.noreply.github.com>
2025-02-12 22:55:27 -0500

    Update run_server_linux.sh

--- ConfigServer/run_server_linux.sh
+++ ConfigServer/run_server_linux.sh
@@ -70,8 +70,10 @@ clear
 printf '\e[1;44m%-6s\e[m'
 printf '\n'
 printf '\e[1;44m%-6s\e[m' " "
+printf '                                                                   \n'
+printf '\e[1;44m%-6s\e[m' " "
 printf '\e[1;44m%-6s\e[m' 
-printf '                                     \n'
+printf '                                                                   \n'
 printf '\n'
 printf '\e[1;44m%-6s\e[m' " "
 printf '                                                                   \n'
@@ -80,15 +82,15 @@ printf '                    Developed by the xDrip team                    \n'
 printf '\e[1;44m%-6s\e[m' " "
 printf '                                                                   \n'
 printf '\e[1;44m%-6s\e[m' " "
-   printf '  Click on this \e]8;;%s\e\\link\e]8;;\e\\ to open a web page for editing variables.     \n' "$printfVar"
-   printf '\e[1;44m%-6s\e[m' " "
-   printf '                                                                   \n'
-   printf '\e[1;44m%-6s\e[m' " "
-   printf '                                                                   \n'
-   printf '\e[1;44m%-6s\e[m' " "
-   printf '  The server will run for 15 minutes and then stop if not used.     \n'
-   printf '\e[1;44m%-6s\e[m' " "
-   printf 'Press Ctrl + C to stop it earlier.\n'
+printf '  Click on this \e]8;;%s\e\\link\e]8;;\e\\ to open a web page for editing variables.     \n' "$printfVar"
+printf '\e[1;44m%-6s\e[m' " "
+printf '                                                                   \n'
+printf '\e[1;44m%-6s\e[m' " "
+printf '                                                                   \n'
+printf '\e[1;44m%-6s\e[m' " "
+printf '  The server will run for 15 minutes and then stop if not used.     \n'
+printf '\e[1;44m%-6s\e[m' " "
+printf '  Press Ctrl + C to stop it earlier.                                \n'
  #  printf 'Press any key to return to the main menu.\n'
    
 #   printf '\e[1;44m%-6s\e[m' " "
