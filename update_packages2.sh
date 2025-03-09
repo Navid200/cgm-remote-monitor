95bf419197f5ab2d728495f2e95745eda785e63c
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-09 11:39:40 -0400

    Update update_packages2.sh

--- update_packages2.sh
+++ update_packages2.sh
@@ -4,15 +4,18 @@ echo
 echo "Install packages after the installation of Nightscout. - Navid200"
 echo
 
+# List of packages installed here:
+# screen  nano  qrencode  file  vis  lsb-release  apt-transport-https  ca-certificates  python3-pip  python3-django  python3-django-extensions  python3-werkzeug  python3-qrcode
+
 # Let's upgrade packages if available and install the missing needed packages.
-sudo apt-get update
+sudo apt-get -o DPkg::Lock::Timeout=30 update
 
 # packages
 whichpack=$(which file)
 if [ "$whichpack" = "" ]
 then
-  sudo apt-get -y install screen nano qrencode file vis lsb-release apt-transport-https ca-certificates 
-  sudo apt-get -y install python3-pip
+  sudo apt-get -o DPkg::Lock::Timeout=30 -y install screen nano qrencode file vis lsb-release apt-transport-https ca-certificates 
+  sudo apt-get -o DPkg::Lock::Timeout=30 -y install python3-pip
   sudo apt -y install python3-django python3-django-extensions python3-werkzeug python3-qrcode
 fi 
 
