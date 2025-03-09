aeb69cc1db4887ebf2c95d892216f9c66369f34c
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-09 16:58:17 -0400

    Update update_packages2.sh

--- update_packages2.sh
+++ update_packages2.sh
@@ -8,14 +8,14 @@ echo
 # screen  nano  qrencode  file  vis  lsb-release  apt-transport-https  ca-certificates  python3-pip  python3-django  python3-django-extensions  python3-werkzeug  python3-qrcode
 
 # Let's upgrade packages if available and install the missing needed packages.
-sudo apt-get -o DPkg::Lock::Timeout=30 update
+sudo apt-get -o dpkg::lock::Timeout=120 update
 
 # packages
 whichpack=$(which file)
 if [ "$whichpack" = "" ]
 then
-  sudo apt-get -o DPkg::Lock::Timeout=30 -y install screen nano qrencode file vis lsb-release apt-transport-https ca-certificates 
-  sudo apt-get -o DPkg::Lock::Timeout=30 -y install python3-pip
+  sudo apt-get -o dpkg::lock::Timeout=120 -y install screen nano qrencode file vis lsb-release apt-transport-https ca-certificates 
+  sudo apt-get -o dpkg::lock::Timeout=120 -y install python3-pip
   sudo apt -y install python3-django python3-django-extensions python3-werkzeug python3-qrcode
 fi 
 
