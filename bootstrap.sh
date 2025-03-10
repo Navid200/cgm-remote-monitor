552fa837e3fab0b96b95335d91e767dbd74442c3
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-10 13:51:40 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -93,14 +93,14 @@ sudo mkdir scripts
 cd /srv
 sudo rm -rf *
 echo "Please be patient."
-sudo git clone https://github.com/jamorham/nightscout-vps.git  # ✅✅✅✅✅ Main - Uncomment before PR.
-#sudo git clone https://github.com/Navid200/cgm-remote-monitor.git  # ⛔⛔⛔⛔⛔ For test - Comment out before PR.
+#sudo git clone https://github.com/jamorham/nightscout-vps.git  # ✅✅✅✅✅ Main - Uncomment before PR.
+sudo git clone https://github.com/Navid200/cgm-remote-monitor.git  # ⛔⛔⛔⛔⛔ For test - Comment out before PR.
 
 ls > /tmp/repo
 sudo mv -f /tmp/repo .    # The repository name is now in /srv/repo
 cd "$(< repo)"
-sudo git checkout vps-dev  # ✅✅✅✅✅ Main - Uncomment before PR.
-#sudo git checkout No_ok_Reboot_counter  # ⛔⛔⛔⛔⛔ For test - Comment out before PR.
+#sudo git checkout vps-dev  # ✅✅✅✅✅ Main - Uncomment before PR.
+sudo git checkout Wait_for_completion  # ⛔⛔⛔⛔⛔ For test - Comment out before PR.
 
 sudo git branch > /tmp/branch
 grep "*" /tmp/branch | awk '{print $2}' > /tmp/brnch
