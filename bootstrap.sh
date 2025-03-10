c531333c80a9240d93c6e62877b0f3e0bf6e7e86
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-09 22:52:35 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,8 +17,8 @@ echo
 # Regardless, we need to remember this destructive nature of bootstrap.
 # One must always run Install Nightscout phase 1 after running bootstrap.
 
-sudo apt-get -o dpkg::lock::Timeout=120 update
-sudo apt-get -o dpkg::lock::Timeout=120 install dialog
+sudo apt-get -o dpkg::lock::Timeout=120 -o apt::lock::Timeout=120 update
+sudo apt-get install dialog
 
 ubversion="$(cat /etc/issue | awk '{print $2}')"
 
@@ -66,7 +66,7 @@ then
   exit
 fi
 
-  sudo apt-get -o dpkg::lock::Timeout=120 install -y  git 
+  sudo apt-get -o dpkg::lock::Timeout=120 -o apt::lock::Timeout=120 install -y  git 
 
 if [ ! -s /xDrip ]
 then
