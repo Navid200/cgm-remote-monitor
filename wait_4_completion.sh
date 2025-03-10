4a3b7dde67b6e54325bdeceef489b6af9f22dadf
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-10 09:33:56 -0400

    Update wait_4_completion.sh

--- wait_4_completion.sh
+++ wait_4_completion.sh
@@ -9,14 +9,14 @@ apt_present=1
 while [ $apt_present -gt 0 ]
 do
 
-ps aux | grep '[a]pt' > /tmp/running_apt
-if [ -s /tmp/running_apt ]
+running_apt=$(ps aux | grep '[a]pt') 
+if [ "$running_apt" = "" ]
 then
+  apr_present=0
+else
   echo " $count  Waiting for installation to complete"
   sleep 10
   count = count + 10
-else
-  apr_present=0
 fi
 
 done
