78f8aa5d16ba1275ae7ae6053da6b4e1e7265911
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-10 17:16:21 -0400

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -30,6 +30,7 @@ fi
 whichpack="$(mongod --version | sed -n 1p)"
 if [ ! "${whichpack%%.*}" = "db version v8" ]
 then
+  /xDrip/scripts/wait_4_completion.sh
   curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg --dearmor
   echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list   
   /xDrip/scripts/wait_4_completion.sh
@@ -54,6 +55,7 @@ if [ ! "${whichpack%%.*}" = "v16" ]
 then
 /xDrip/scripts/wait_4_completion.sh
 sudo /xDrip/scripts/nodesource_setup.sh
+/xDrip/scripts/wait_4_completion.sh
 sudo apt-get install nodejs -y
 # Nightscout needs version 6 of npm.  So, we are going to install that version now effectivwely downgrading it.  
 sudo npm install -g npm@6.14.18
