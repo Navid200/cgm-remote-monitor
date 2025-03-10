dfea45eb2ea099fa8eafad451e6658a7a1f43f26
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-10 09:17:06 -0400

    Update wait_4_completion.sh

--- wait_4_completion.sh
+++ wait_4_completion.sh
@@ -4,8 +4,19 @@ echo
 echo "Verifying install completion"
 echo
 
+count=0
+apt_present=1
+while [ $apt_present -gt 0 ]
+do
+
 ps aux | grep '[a]pt' > /tmp/running_apt
 if [ -s /tmp/running_apt ]
 then
-  echo "apt is running"
+  echo " $count  Waiting for installation to complete"
+  sleep 10
+  count = count + 10
+else
+  apr_present=0
 fi
+
+done
