61d4f64671133d8e4b3852daa27fc24a4dd4c17d
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-10 10:33:19 -0400

    Update wait_4_completion.sh

--- wait_4_completion.sh
+++ wait_4_completion.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 echo
-echo "Verifying install completion"
+echo "Verifying installation completion"
 echo
 
 count=0
 apt_present=1
 while [ $apt_present -gt 0 ]
 do
-
 running_apt=$(ps aux | grep '[a]pt') 
 if [ "$running_apt" = "" ]
 then
-  apr_present=0
+  apt_present=0
 else
-  echo " $count  Waiting for installation to complete"
-  sleep 10
-  count = count + 10
+  echo " $count  Waiting for installations to complete"
+  sleep 15
+  count=$((count+15))
 fi
-
 done
+ 
