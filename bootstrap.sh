71693535016be11c9bac5fd72584954ca9bf6e7e
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-10 09:59:53 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,11 +26,11 @@ do
 running_apt=$(ps aux | grep '[a]pt') 
 if [ "$running_apt" = "" ]
 then
-  apr_present=0
+  apt_present=0
 else
   echo " $count  Waiting for installation to complete"
-  sleep 10
-  count=count+10
+  sleep 15
+  count=$count+15
 fi
 
 done
