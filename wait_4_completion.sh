6e9db1244c13badef7ad6b7f36efcd77ce6f107a
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-10 13:05:52 -0400

    Create wait_4_completion.sh

---
+++ wait_4_completion.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo
+echo "Waiting for installations to complete"
+echo
+
+####### Keep this file synchronized with the marked contents of the bootstrap ###
+####################### Wait for updates to complete
+
+count=0
+apt_present=1
+while [ $apt_present -gt 0 ]
+do
+running_apt=$(ps aux | grep '[a]pt') 
+if [ "$running_apt" = "" ]
+then
+  apt_present=0
+else
+  echo " $count  Waiting for installations to complete"
+  sleep 10
+  count=$((count+10))
+fi
+done
+######################################################################
+ 
