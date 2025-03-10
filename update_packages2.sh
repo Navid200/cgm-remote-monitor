d8fb9ab3d7e6347edc52659cfc823d53f80e504e
Navid <51497406+Navid200@users.noreply.github.com>
2025-03-10 10:54:50 -0400

    Update update_packages2.sh

--- update_packages2.sh
+++ update_packages2.sh
@@ -8,20 +8,20 @@ echo
 # screen  nano  qrencode  file  vis  lsb-release  apt-transport-https  ca-certificates  python3-pip  python3-django  python3-django-extensions  python3-werkzeug  python3-qrcode
 
 # Let's upgrade packages if available and install the missing needed packages.
-sudo apt-get -o dpkg::lock::Timeout=120 update
+/xDrip/scripts/wait_4_completion.sh
+apt-get -o dpkg::lock::Timeout=120 update
 
 # packages
 whichpack=$(which file)
 if [ "$whichpack" = "" ]
 then
-  sudo apt-get -o dpkg::lock::Timeout=120 -y install screen nano qrencode file vis lsb-release apt-transport-https ca-certificates bind9-dnsutils 
-  sudo apt-get -o dpkg::lock::Timeout=120 -y install python3-pip
-  sudo apt -y install python3-django python3-django-extensions python3-werkzeug python3-qrcode
+  apt-get -o dpkg::lock::Timeout=120 -y install screen nano qrencode file vis lsb-release apt-transport-https ca-certificates bind9-dnsutils 
+  apt-get -o dpkg::lock::Timeout=120 -y install python3-pip
+  apt -y install python3-django python3-django-extensions python3-werkzeug python3-qrcode
 fi 
 
 # The last item on the above list of packages must be verified in Status.sh to have been installed.  
 
 # Add log
 /xDrip/scripts/AddLog.sh "The secondary packages have been installed" /xDrip/Logs
-
   
