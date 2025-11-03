afedd02c9e5e8ac6dcdc520eff645d2f05c787d8
Navid <51497406+Navid200@users.noreply.github.com>
2025-11-02 23:49:26 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -75,11 +75,11 @@ The Ubuntu installation option is incorrect. Please refer to the guide for detai
 fi 
 
 clear
-dialog --colors --yesno "              \Zr Google Cloud Nightscout \Zn\n\n\n\
+dialog --colors --yesno "              \Zr Google Cloud Nightscout \Zn\n\n\
 This software and its associated online instructions are provided “as is,” without any warranties, express or implied. By using this software, you accept full responsibility and assume all risks associated with its use.\n\n\
 The developers and contributors shall not be held liable for any damages, losses, or other consequences arising from the use of this software or its documentation.\n\n\
 Before using this software, consult a qualified healthcare professional to determine its suitability for your specific needs.\n\n\
-I understand and agree." 18 62
+I understand and agree." 19 62
 response=$?
 if [ $response = 255 ] || [ $response = 1 ]
 then
