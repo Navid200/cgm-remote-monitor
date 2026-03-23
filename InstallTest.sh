5e6a6d17a0862434601bd1940619f133ff1c878b
Navid <51497406+Navid200@users.noreply.github.com>
2026-03-23 18:56:07 -0400

    Create InstallTest.sh

---
+++ InstallTest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo
+echo "JamOrHam Nightscout Installer - Designed for Google Compute Minimal Ubuntu 20 micro instance"
+echo
+
+if [ "`id -u`" != "0" ]
+then
+echo "Script needs root - use sudo bash NS_Install.sh"
+echo "Cannot continue.."
+exit 5
+fi
+
+
+cd /srv 
+
+/xDrip/scripts/wait_4_completion.sh
+apt-get update || apt-get update
+
+npm install 
+
+
+  
